de3e41a0bef6f3642e1403b08e370ca73b8c0691
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2022-12-19 13:13:09 +0530

    Update imdb.py

--- MerissaRobot/Modules/imdb.py
+++ MerissaRobot/Modules/imdb.py
@@ -5,47 +5,33 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from MerissaRobot import pbot
 
 
-@pbot.on_message(filters.command("imdb"))
-async def imdb(client, message):
+@pbot.on_message(filters.command(["imdb", "tmdb"]))
+async def imdb(_, message):
     if len(message.command) < 2:
-        return await message.reply_text("Give me some Movie Name\n\nEx. /imdb Kgf 2")
+        return await message.reply_text("Give me some Movie Name\n\nEx. /imdb Kgf")
     text = (
         message.text.split(None, 1)[1]
         if len(message.command) < 3
         else message.text.split(None, 1)[1].replace(" ", "%20")
     )
-    url = requests.get(f"https://api.safone.tech/tmdb?query={text}").json()["results"][
-        0
-    ]
-    poster = url["poster"]
-    imdb_link = url["imdbLink"]
-    title = url["title"]
-    rating = url["rating"]
-    releasedate = url["releaseDate"]
-    description = url["overview"]
-    image = url["backdrop"]
-    popularity = url["popularity"]
-    runtime = url["runtime"]
-    status = url["status"]
-    await client.send_photo(
-        message.chat.id,
-        poster,
-        caption=f"""[🎥]({image})Imdb Movie Details:
-
-Title = {title}
-Description = {description}
-Rating = {rating}
-Release-Date = {releasedate}
-Popularity = {popularity}
-Runtime = {runtime}
-Status = {status}
+    url = requests.get(f"https://api.safone.me/tmdb?query={text}").json()["results"][0]
+    await message.reply_photo(
+        photo=url["poster"],
+        caption=f"""**IMDB Movie Details :**
+**Title :** {url["title"]}
+**Description :** {url["overview"]}
+**Rating :** {url["rating"]}
+**Release-Date :** {url["releaseDate"]}
+**Popularity :** {url["popularity"]}
+**Runtime :** {url["runtime"]}
+**Status :** {url["status"]}
 """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        text="Imdb link",
-                        url=imdb_link,
+                        text="IMDB Link",
+                        url=url["imdbLink"],
                     ),
                 ],
             ],
