be630ba1498883c54b937c9df4f075a7187ace57
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2022-12-25 14:45:55 +0530

    Create antinsfw_mongo.py

---
+++ MerissaRobot/Database/mongo/antinsfw_mongo.py
@@ -0,0 +1,26 @@
+import codecs
+import pickle
+from typing import Dict, List, Union
+from MerissaRobot.Database.mongo import db
+
+nsfwdb = db.nsfw
+
+async def is_nsfw_on(chat_id: int) -> bool:
+    chat = nsfwdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return True
+    return False
+
+
+async def nsfw_on(chat_id: int):
+    is_nsfw = is_nsfw_on(chat_id)
+    if is_nsfw:
+        return
+    return nsfwdb.delete_one({"chat_id": chat_id})
+
+
+async def nsfw_off(chat_id: int):
+    is_nsfw = is_nsfw_on(chat_id)
+    if not is_nsfw:
+        return
+    return nsfwdb.insert_one({"chat_id": chat_id})
