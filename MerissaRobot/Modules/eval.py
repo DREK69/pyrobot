bfcb5969988440c91c25138c367c4b15c69c7aca
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2022-12-25 08:07:54 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Modules/eval.py
+++ MerissaRobot/Modules/eval.py
@@ -9,9 +9,9 @@ from time import time
 
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from Rani import app
 
 from config import OWNER_ID as SUDO_USER
-from Rani import app
 
 
 async def aexec(code, client, message):
