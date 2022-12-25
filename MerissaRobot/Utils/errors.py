b28aeba5a981ce15c0415ea2ceca4bcf8d038fe2
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2022-12-25 14:59:39 +0530

    Update errors.py

--- MerissaRobot/Utils/errors.py
+++ MerissaRobot/Utils/errors.py
@@ -4,8 +4,8 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from MerissaRobot import EVENT_LOGS
-from MerissaRobot import pbot as app
+from MerissaRobot import OWNER_ID
+from MerissaRobot import pbot 
 
 
 def split_limits(text):
@@ -33,12 +33,11 @@ def capture_err(func):
         try:
             return await func(client, message, *args, **kwargs)
         except ChatWriteForbidden:
-            await app.leave_chat(message.chat.id)
             return
         except Exception as err:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             errors = traceback.format_exception(
-                etype=exc_type,
+                exc_type,
                 value=exc_obj,
                 tb=exc_tb,
             )
@@ -51,7 +50,7 @@ def capture_err(func):
                 ),
             )
             for x in error_feedback:
-                await app.send_message(EVENT_LOGS, x)
+                await pbot.send_message(OWNER_ID, x)
             raise err
 
     return capture
