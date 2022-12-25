2a3144ad580b5724a2326aa9109af47947592335
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2022-12-25 08:09:15 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Modules/eval.py
+++ MerissaRobot/Modules/eval.py
@@ -9,9 +9,9 @@ from time import time
 
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from MerissaRobot import pbot as app
 
 from config import OWNER_ID as SUDO_USER
+from MerissaRobot import pbot as app
 
 
 async def aexec(code, client, message):
