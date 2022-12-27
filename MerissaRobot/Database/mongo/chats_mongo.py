f7ce71d1cbb124248f28589f68b1d96495c40cd7
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2022-12-27 11:56:16 +0530

    Create chats_mongo.py

---
+++ MerissaRobot/Database/mongo/chats_mongo.py
@@ -0,0 +1,12 @@
+from MerissaRobot.Database.mongo import db
+
+chatsdb = db.chats
+
+async def get_served_chats() -> list:
+    chats = chatsdb.find({"chat_id": {"$lt": 0}})
+    if not chats:
+        return []
+    chats_list = []
+    for chat in await chats.to_list(length=1000000000):
+        chats_list.append(chat)
+    return chats_list
