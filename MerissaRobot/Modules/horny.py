26a9d7415036e9a31768bb83089ff2b878cc47ff
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-01-11 16:20:56 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Modules/horny.py
+++ MerissaRobot/Modules/horny.py
@@ -202,4 +202,6 @@ async def boobs(client, message):
 @bot.on_callback_query(filters.regex("boob"))
 async def memess(_, query: CallbackQuery):
     res = requests.get("https://api.prince-xd.ml/boobs").json()
-    await query.edit_message_media(InputMediaPhoto(res), caption="Powered by @MerissaRobot")
+    await query.edit_message_media(
+        InputMediaPhoto(res), caption="Powered by @MerissaRobot"
+    )
