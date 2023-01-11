a0f5e9dbb2725984b1307e24d7d905fc3a620987
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-01-11 21:25:45 +0530

    Update horny.py

--- MerissaRobot/Modules/horny.py
+++ MerissaRobot/Modules/horny.py
@@ -200,7 +200,6 @@ async def boobs(client, message):
 
 
 @bot.on_callback_query(filters.regex("boob"))
-async def memess(_, query: CallbackQuery):
-    query = query.message
+async def memess(_, query: CallbackQuery):   
     res = requests.get("https://api.prince-xd.ml/boobs").json()
     await query.edit_message_media(res)
