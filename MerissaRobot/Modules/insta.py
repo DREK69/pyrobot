95ae730ce638e4208d228d0527c8506b5858c100
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-01-12 15:31:08 +0530

    Create insta.py

---
+++ MerissaRobot/Modules/insta.py
@@ -0,0 +1,19 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from requests import get 
+from MerissaRobot import pbot
+
+
+@pbot.on_message(filters.command("insta"))
+async def instadown(_, message):
+    if len(message.command) < 2:
+        return await message.reply_text("Give me some link\n\nEx. /insta link")
+    name = (
+        message.text.split(None, 1)[1]
+        if len(message.command) < 3
+        else message.text.split(None, 1)[1].replace(" ", "%20")
+    )
+    x = get(f"https://api.princexd.tech/igdown?link={name}").json()
+    await message.reply_video(
+        x, caption="Powered by @MerissaRobot",       
+    )
