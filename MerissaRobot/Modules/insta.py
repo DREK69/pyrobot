63d27749ef4b9c8d4d7301b0da047f0e643c615a
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-01-12 16:26:32 +0530

    Update insta.py

--- MerissaRobot/Modules/insta.py
+++ MerissaRobot/Modules/insta.py
@@ -18,3 +18,18 @@ async def instadown(_, message):
         x,
         caption="Powered by @MerissaRobot",
     )
+
+@pbot.on_message(filters.command("insta2"))
+async def instadown(_, message):
+    if len(message.command) < 2:
+        return await message.reply_text("Give me some link\n\nEx. /insta link")
+    name = (
+        message.text.split(None, 1)[1]
+        if len(message.command) < 3
+        else message.text.split(None, 1)[1].replace(" ", "%20")
+    )
+    x = get(f"https://api.princexd.tech/igdown?link={name}").json()[0]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    )
