cb8d00daa72bc5fed06f3ac5a0723ce848baa5ee
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-01-15 19:01:19 +0530

    Update __init__.py

--- MerissaRobot/Database/mongo/__init__.py
+++ MerissaRobot/Database/mongo/__init__.py
@@ -1,9 +1,16 @@
 from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
 from MerissaRobot import MONGO_DB_URI
+
+
+
 
 MONGO_DB = "mongodb+srv://merissa:robot@cluster0.4yifs.mongodb.net/?retryWrites=true&w=majority"
 
 client = MongoClient(MONGO_DB_URI)
 leveldb = MongoClient(MONGO_DB)
+
+mongo = MongoCli(MONGO_DB)
+afkdb = mongo.MerissaRobot
 db = client["MerissaRobot"]
