7d132e0db9483ae78841a829ecf99949715a8488
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-01-15 13:31:46 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Database/mongo/__init__.py
+++ MerissaRobot/Database/mongo/__init__.py
@@ -1,10 +1,7 @@
-from pymongo import MongoClient
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+from pymongo import MongoClient
 
 from MerissaRobot import MONGO_DB_URI
-
-
-
 
 MONGO_DB = "mongodb+srv://merissa:robot@cluster0.4yifs.mongodb.net/?retryWrites=true&w=majority"
 
