80c731e6ee5208a51a8690bbbebf0ef8fbc5287c
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-01-16 00:43:09 +0530

    Update afk_mongo.py

--- MerissaRobot/Database/mongo/afk_mongo.py
+++ MerissaRobot/Database/mongo/afk_mongo.py
@@ -1,21 +1,6 @@
 from MerissaRobot.Database.mongo import afkdb as db
 
 afkdb = db.afkdb
-chatsdb = db.chatsdb
-
-async def is_served_chat(chat_id: int) -> bool:
-    chat = await chatsdb.find_one({"chat_id": chat_id})
-    if not chat:
-        return False
-    return True
-
-
-async def add_served_chat(chat_id: int):
-    is_served = await is_served_chat(chat_id)
-    if is_served:
-        return
-    return await chatsdb.insert_one({"chat_id": chat_id})
-
 
 async def is_afk(user_id: int) -> bool:
     user = await afkdb.find_one({"user_id": user_id})
