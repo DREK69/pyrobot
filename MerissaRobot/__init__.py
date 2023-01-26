0fc93b1114ecc0a8abcac348e191284e2e03587b
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-01-26 14:29:06 +0530

    Update __init__.py

--- MerissaRobot/__init__.py
+++ MerissaRobot/__init__.py
@@ -11,7 +11,6 @@ from aiohttp import ClientSession
 from loguru import logger
 from pyrogram import Client, errors
 from pyrogram.types import Message
-from pyromod import listen  # ignore it
 from Python_ARQ import ARQ
 from redis import StrictRedis
 from telethon import TelegramClient
