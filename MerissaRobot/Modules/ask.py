f4714ca92a363cc5a6c15a284fa4555a60938d9f
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-01-30 23:30:28 +0530

    Update ask.py

--- MerissaRobot/Modules/ask.py
+++ MerissaRobot/Modules/ask.py
@@ -7,7 +7,7 @@ from MerissaRobot import pbot
 @pbot.on_message(filters.command("ask"))
 async def instadown(_, message):
     if len(message.command) < 2:
-        return await message.reply_text("Give me some link\n\nEx. /insta link")
+        return await message.reply_text("Give me some link\n\nEx. /ask question")
     query = (
         message.text.split(None, 1)[1]
         if len(message.command) < 3
