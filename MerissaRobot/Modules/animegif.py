4b162621a7d6cb1f765053081728f7296aac523a
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-01-31 15:25:56 +0530

    Create animegif.py

---
+++ MerissaRobot/Modules/animegif.py
@@ -0,0 +1,252 @@
+import requests 
+
+from pyrogram import filters
+from MerissaRobot import pbot
+
+@pbot.on_message(filters.command("hug"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/hug").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("cuddle"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/cuddle").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("poke"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/poke").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("facepalm"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/facepalm").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("stare"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/stare").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("pout"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/pout").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("handhold"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/handhold").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("wave"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/wave").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("blush"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/blush").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("dance"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/dance").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("baka"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/baka").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("bore"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/bare").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("laugh"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/laugh").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("smug"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/smug").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("thumbsup"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/thumbsup").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("shoot"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/shoot").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("tickle"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/tickle").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("feed"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/feed").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("think"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/think").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("wink"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/wink").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("sleep"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/sleep").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("punch"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/punch").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("cry"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/cry").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("kill"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/kill").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("smile"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/smile").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("highfive"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/highfive").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("kick"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/kick").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("slap"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/slap").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("pat"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/pat").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("boobs"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/boobs").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
+
+@pbot.on_message(filters.command("hentai"))
+async def animegif(_, message):
+    x = get(f"https://api.princexd.tech/anime/hentai").json()["url"]
+    await message.reply_video(
+        x,
+        caption="Powered by @MerissaRobot",
+    ) 
