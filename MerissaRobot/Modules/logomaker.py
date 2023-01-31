a3c5e98c24b27a54503dbf4e4a9616132e6d3a7c
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-01-31 18:01:56 +0530

    Update logomaker.py

--- MerissaRobot/Modules/logomaker.py
+++ MerissaRobot/Modules/logomaker.py
@@ -9,12 +9,60 @@ from MerissaRobot import telethn as bot
 from MerissaRobot.events import register
 
 
+def mediainfo(media):
+    xx = str((str(media)).split("(", maxsplit=1)[0])
+    m = ""
+    if xx == "MessageMediaDocument":
+        mim = media.document.mime_type
+        if mim == "application/x-tgsticker":
+            m = "sticker animated"
+        elif "image" in mim:
+            if mim == "image/webp":
+                m = "sticker"
+            elif mim == "image/gif":
+                m = "gif as doc"
+            else:
+                m = "pic as doc"
+        elif "video" in mim:
+            if "DocumentAttributeAnimated" in str(media):
+                m = "gif"
+            elif "DocumentAttributeVideo" in str(media):
+                i = str(media.document.attributes[0])
+                if "supports_streaming=True" in i:
+                    m = "video"
+                m = "video as doc"
+            else:
+                m = "video"
+        elif "audio" in mim:
+            m = "audio"
+        else:
+            m = "document"
+    elif xx == "MessageMediaPhoto":
+        m = "pic"
+    elif xx == "MessageMediaWebPage":
+        m = "web"
+    return m
+
+
 @register(pattern="^/logo ?(.*)")
 async def makelogo(event):
     quew = event.pattern_match.group(1).strip()
     if not quew:
         await event.reply("Provide Some Text To Draw! Example: /logo <your name>")
         return
+    randBg, randFont = "", ""
+    if event.reply_to_msg_id:
+        temp = await event.get_reply_message()
+        if temp.media:
+            if hasattr(temp.media, "document"):
+                if "font" in temp.file.mime_type:
+                    randFont = await temp.download_media()
+                elif (".ttf" in temp.file.name) or (".otf" in temp.file.name):
+                    randFont = await temp.download_media()
+            elif "pic" in mediainfo(temp.media):
+                randBg = await temp.download_media()
+    else:
+        pass
     msg = await event.reply("Creating your logo...wait!")
     try:
         text = quew
@@ -87,6 +135,8 @@ async def makelogo(event):
             "https://telegra.ph/file/550aa046ab930967bc377.jpg",
             "https://telegra.ph/file/7c1696bc94225779d656a.jpg",
         ]
+        if not randBg:
+            randBg = random.choice(bg)
 
         allFonts = [
             "font(1).otf",
