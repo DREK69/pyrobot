9ed9fe8ed0e662f93ada694f08e72b6ab1f3e03b
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-02-01 15:58:03 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Modules/core.py
+++ MerissaRobot/Modules/core.py
@@ -6,12 +6,13 @@ from MerissaRobot.events import register
 
 client = tbot
 
+
 @register(pattern=r"^/send ?(.*)")
 async def Prof(event):
     if event.sender_id == OWNER_ID:
         pass
     else:
-        return    
+        return
     message_id = event.message.id
     input_str = event.pattern_match.group(1)
     the_plugin_file = "./MerissaRobot/Modules/{}.py".format(input_str)
@@ -21,7 +22,7 @@ async def Prof(event):
             event.chat_id,
             the_plugin_file,
             force_document=True,
-            allow_cache=False,           
+            allow_cache=False,
             reply_to=message_id,
         )
     else:
