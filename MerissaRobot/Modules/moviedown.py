9ef70194be81e75dd8c58285581b32247a0ea5ee
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-02-07 17:05:33 +0530

    Update moviedown.py

--- MerissaRobot/Modules/moviedown.py
+++ MerissaRobot/Modules/moviedown.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 from telegram.ext import CallbackQueryHandler, CommandHandler
 
 from MerissaRobot import dispatcher
@@ -76,9 +76,9 @@ def movie_result(update, context) -> None:
     caption = f"⚡ Fast Download Links :-\n\n{link}"
     if len(caption) > 4095:
         for x in range(0, len(caption), 4095):
-            query.message.ediy_text(text=caption[x : x + 4095], reply_markup=None)
+            query.message.ediy_text(text=caption[x : x + 4095], reply_markup=None, parse_mode=ParseMode.MARKDOWN)
     else:
-        query.message.edit_text(text=caption, reply_markup=None)
+        query.message.edit_text(text=caption, reply_markup=None, parse_mode=ParseMode.MARKDOWN)
 
 
 dispatcher.add_handler(CommandHandler("movie", find_movie))
