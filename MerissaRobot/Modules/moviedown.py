afcbfc6a00cfce486c5e97392899094312c36755
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-02-07 19:10:13 +0530

    Update moviedown.py

--- MerissaRobot/Modules/moviedown.py
+++ MerissaRobot/Modules/moviedown.py
@@ -7,7 +7,6 @@ from MerissaRobot import dispatcher
 
 url_list = {}
 
-
 def search_movies(query):
     movies_list = []
     movies_details = {}
@@ -73,6 +72,7 @@ def movie_result(update, context) -> None:
     links = s["links"]
     for i in links:
         link += f"🎬{i}\n[Click Here To Download]({links[i]})\n\n"
+        link += "Credits To MKVCinemas\nPowered By @MerissaRobot"
     caption = f"⚡ Fast Download Links :-\n\n{link}"
     if len(caption) > 4095:
         for x in range(0, len(caption), 4095):
