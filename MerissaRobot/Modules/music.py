c72982ec7a8c7c1f7d077b8ebbecce6577a27ab6
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-02-13 22:29:49 +0530

    Update music.py

--- MerissaRobot/Modules/music.py
+++ MerissaRobot/Modules/music.py
@@ -32,6 +32,7 @@ def song(client, message):
     global chat_id
     chat_id = message.chat.id
     global link
+    global thumbnail 
     user_id = message.from_user.id
     user_name = message.from_user.first_name
     user = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
@@ -77,7 +78,7 @@ async def callback_query(Client, CallbackQuery):
             "Downloading And Uploading Started\n\nDownload And Upload Speed could be slow. Please hold on.."
         )
         title = youtube_audio.title
-        med = InputMediaAudio(media=audio_file, caption=title, title=title)
+        med = InputMediaAudio(media=audio_file, caption=title, title=title, thumb=thumbnail)
         try:
             await CallbackQuery.edit_message_media(media=med)
         except Exception as error:
