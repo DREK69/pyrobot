1e80aeeadfff786f7d82909b9149cfd139d65be6
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-02-17 20:42:42 +0530

    Update asupan.py

--- MerissaRobot/Modules/asupan.py
+++ MerissaRobot/Modules/asupan.py
@@ -1,4 +1,5 @@
-import requests
+import os
+import wget
 from pyrogram import filters
 
 from MerissaRobot import pbot as bot
@@ -7,6 +8,7 @@ from MerissaRobot import pbot as bot
 @bot.on_message(filters.command("asupan"))
 async def animememes(_, message):
     x = await message.reply_text("Please Wait Video Uploading...")
-    res = requests.get("https://api.princexd.tech/asupan/tiktok").json()["url"]
-    await message.reply_video(res)
+    res = wget.download("https://api.princexd.tech/asupan/tiktok", "asupan.mp4")
+    await message.reply_video("asupan.mp4")
     await x.delete()
+    os.remove("asupan.mp4")
