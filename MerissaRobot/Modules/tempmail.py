f284da6e3fe2cca40f38da4eb3df9c53a0568308
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-02-18 13:44:56 +0530

    Update tempmail.py

--- MerissaRobot/Modules/tempmail.py
+++ MerissaRobot/Modules/tempmail.py
@@ -1,152 +1,159 @@
 import os
 
 import requests as re
-import wget
 from pyrogram import *
+import os
+import asyncio
+import requests
+import random
+import bs4
+
+from pykeyboard import InlineKeyboard
+from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid, bad_request_400
+from RandomWordGenerator import RandomWord
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from MerissaRobot import pbot as app
 
-buttons = InlineKeyboardMarkup(
-    [
-        [InlineKeyboardButton("📧 Generate Email", callback_data="generate")],
-        [
-            InlineKeyboardButton("🔁 Refresh", callback_data="refresh"),
-            InlineKeyboardButton("🗑️ Close", callback_data="cb_close"),
-        ],
-    ]
-)
 
-msg_buttons = InlineKeyboardMarkup(
-    [
-        [InlineKeyboardButton("📫 View message", callback_data="view_msg")],
-        [InlineKeyboardButton("🗑️ Close", callback_data="cb_close")],
-    ]
-)
+#********************************************************************************
 
-email = ""
+API1='https://www.1secmail.com/api/v1/?action=getDomainList'
+API2='https://www.1secmail.com/api/v1/?action=getMessages&login='
+API3='https://www.1secmail.com/api/v1/?action=readMessage&login='
 
+#********************************************************************************
+
+create = InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Groupdcbots ⚡", url="https://t.me/NotReallyPrince")]])
+
+#*******************************************************************************
 
 @app.on_message(filters.command("genmail"))
-async def start_msg(client, message):
-    await message.reply(
-        "Hello Welcome to Merissa Temp Mail Generator", reply_markup=buttons
+async def fakemailgen(_, message: Message):
+    name = message.from_user.id
+    m =  await app.send_message(name,text=f"📧 Creating  temp email....",reply_markup = create)
+    rp = RandomWord(max_word_size=8, include_digits=True)
+    email = rp.generate()
+    xx = requests.get(API1).json()
+    domain = random.choice(xx)
+    #print(email)
+    mes = await app.send_message(
+    name, 
+    text = f"""
+**📬 Done,Your Email Address Created!**
+📧 **Email** : `{email}@{domain}`
+📨 **Mail BOX** : `empty`
+♨️ **Powered by** : @groupdcbots """,
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("♻️ Update Mail BOX ♻️", callback_data = f"mailbox |{email}|{domain}")]]))
+    pi = await mes.pin(disable_notification=True, both_sides=True)
+    await m.delete()
+    await pi.delete()
+
+async def gen_keyboard(mails, email, domain):
+    num = 0
+    i_kbd = InlineKeyboard(row_width=1)
+    data = []
+    for mail in mails:
+        id = mail['id']
+        data.append(
+            InlineKeyboardButton(f"{mail['subject']}", f"mail |{email}|{domain}|{id}")
+        )
+        num += 1
+    data.append(
+        InlineKeyboardButton(f"Update Mail BOX ♻️", f"mailbox |{email}|{domain}")
     )
+    i_kbd.add(*data)
+    return i_kbd
+ 
+#********************************************************************************
 
-
-@app.on_callback_query()
-async def mailbox(client, message):
-    response = message.data
-    if response == "generate":
-        global email
-        email = re.get(
-            "https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1"
-        ).json()[0]
-        await message.edit_message_text(
-            "__**Your Temporary E-mail: **__`" + str(email) + "`", reply_markup=buttons
-        )
-        print(email)
-    elif response == "refresh":
-        print(email)
+@app.on_callback_query(filters.regex("mailbox"))
+async def mail_box(_, query : CallbackQuery):
+    Data = query.data
+    callback_request = Data.split(None, 1)[1]
+    m, email , domain = callback_request.split("|")
+    mails = requests.get(f'{API2}{email}&domain={domain}').json()
+    if mails == []:
+            await query.answer("🤷‍♂️ No Mails found! 🤷‍♂️")
+    else:
         try:
-            if email == "":
-                await message.edit_message_text(
-                    "Genaerate a email", reply_markup=buttons
-                )
-            else:
-                getmsg_endp = (
-                    "https://www.1secmail.com/api/v1/?action=getMessages&login="
-                    + email[: email.find("@")]
-                    + "&domain="
-                    + email[email.find("@") + 1 :]
-                )
-                print(getmsg_endp)
-                ref_response = re.get(getmsg_endp).json()
-                global idnum
-                idnum = str(ref_response[0]["id"])
-                from_msg = ref_response[0]["from"]
-                subject = ref_response[0]["subject"]
-                refreshrply = (
-                    "You a message from " + from_msg + "\n\nSubject : " + subject
-                )
-                await message.edit_message_text(refreshrply, reply_markup=msg_buttons)
-        except:
-            await message.answer(
-                "No messages were received..\nin your Mailbox " + email
-            )
-    elif response == "view_msg":
-        msg = re.get(
-            "https://www.1secmail.com/api/v1/?action=readMessage&login="
-            + email[: email.find("@")]
-            + "&domain="
-            + email[email.find("@") + 1 :]
-            + "&id="
-            + idnum
-        ).json()
-        print(msg)
-        from_mail = msg["from"]
-        date = msg["date"]
-        subjectt = msg["subject"]
-        attachments = msg["attachments"][0]
-        body = msg["body"]
-        mailbox_view = (
-            "ID No : "
-            + idnum
-            + "\nFrom : "
-            + from_mail
-            + "\nDate : "
-            + date
-            + "\nSubject : "
-            + subjectt
-            + "\n\n"
-            + body
+            smail = f"{email}@{domain}"
+            mbutton = await gen_keyboard(mails,email, domain)
+            await query.message.edit(f""" 
+**📬 Done,Your Email Address Created!**
+📧 **Email** : `{smail}`
+📨 **Mail BOX** : ✅
+**♨️ Powered by** : @Groupdcbots""",
+reply_markup = mbutton
+)   
+        except bad_request_400.MessageNotModified as e:
+            await query.answer("🤷‍♂️ No New Mails found! 🤷‍♂️")
+
+#********************************************************************************
+
+@app.on_callback_query(filters.regex("mail"))
+async def mail_box(_, query : CallbackQuery):
+    Data = query.data
+    callback_request = Data.split(None, 1)[1]
+    m, email , domain, id = callback_request.split("|")
+    mail = requests.get(f'{API3}{email}&domain={domain}&id={id}').json()
+    froms = mail['from']
+    subject = mail['subject']
+    date = mail['date']
+    if mail['textBody'] == "":
+        kk = mail['htmlBody']
+        body = bs4.BeautifulSoup(kk, 'lxml')
+        txt = body.get_text()
+        text = " ".join(txt.split())
+        url_part = body.find('a')
+        link = url_part['href']
+        mbutton = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("🔗 Open Link", url=link)
+                ],
+                [
+                    InlineKeyboardButton("Back", f"mailbox |{email}|{domain}")
+                ]
+            ]
         )
-        await message.edit_message_text(mailbox_view, reply_markup=buttons)
-        print(attachments)
-        mailbox_view = (
-            "ID No : "
-            + idnum
-            + "\nFrom : "
-            + from_mail
-            + "\nDate : "
-            + date
-            + "\nSubject : "
-            + subjectt
-            + "\n\n"
-            + body
+        await query.message.edit(f""" 
+**From:** `{froms}`
+**Subject:** `{subject}`   
+**Date**: `{date}`
+{text}
+""",
+reply_markup = mbutton
+)
+    else:
+        body = mail['textBody']
+        mbutton = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Back", f"mailbox |{email}|{domain}")
+                ]
+            ]
         )
-        if attachments == "[]":
-            await message.edit_message_text(mailbox_view, reply_markup=buttons)
-            await message.answer("No Messages Were Recieved..", show_alert=True)
-        else:
-            dlattach = attachments["filename"]
-            attc = (
-                "https://www.1secmail.com/api/v1/?action=download&login="
-                + email[: email.find("@")]
-                + "&domain="
-                + email[email.find("@") + 1 :]
-                + "&id="
-                + idnum
-                + "&file="
-                + dlattach
-            )
-            print(attc)
-            mailbox_vieww = (
-                "ID No : "
-                + idnum
-                + "\nFrom : "
-                + from_mail
-                + "\nDate : "
-                + date
-                + "\nSubject : "
-                + subjectt
-                + "\n\n"
-                + body
-                + "\n\n"
-                + "[Download]("
-                + attc
-                + ") Attachments"
-            )
-            wget.download(attc)
-            await message.edit_message_text(mailbox_vieww, reply_markup=buttons)
-            os.remove(dlattach)
+        await query.message.edit(f""" 
+**From:** `{froms}`
+**Subject:** `{subject}`   
+**Date**: `{date}`
+{body}
+""",
+reply_markup = mbutton
+)
+#********************************************************************************
+
+@app.on_message(filters.command("domains"))
+async def fakemailgen(_, message: Message):
+    name = message.from_user.id
+    x = requests.get(f'https://www.1secmail.com/api/v1/?action=getDomainList').json()
+    xx = str(",".join(x))
+    email = xx.replace(",", "\n")
+    await app.send_message(
+    name, 
+    text = f"""
+**{email}**
+""",
+    reply_markup = create)
