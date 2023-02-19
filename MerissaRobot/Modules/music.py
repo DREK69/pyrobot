1762831ad02378078e2c568b7b7f44eb4945bdcf
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-02-19 16:35:58 +0530

    Update music.py

--- MerissaRobot/Modules/music.py
+++ MerissaRobot/Modules/music.py
@@ -79,7 +79,8 @@ async def callback_query(Client, CallbackQuery):
     title = youtube_audio.title
     thumb = await CallbackQuery.message.download()
     audio = youtube_audio.streams.filter(only_audio=True).first()
-    song = audio.download()
+    name = f"{youtube_audio.title}.mp3"
+    song = audio.download(filename=name)
     m = await CallbackQuery.edit_message_text(
         "Downloading And Uploading Started\n\nDownload And Upload Speed could be slow. Please hold on.."
     )
