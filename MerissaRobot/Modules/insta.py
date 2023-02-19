3156beb04a0b871ae0e9c5ee1079ae06fc56fb7a
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-02-19 19:56:58 +0530

    Update insta.py

--- MerissaRobot/Modules/insta.py
+++ MerissaRobot/Modules/insta.py
@@ -10,7 +10,7 @@ instaregex = r"^https:\/\/www\.instagram\.com\/(p|tv|reel|stories)\/([A-Za-z0-9\
 async def instadown(_, message):
     name = message.text
     url = get(f"https://api.princexd.tech/igdown?link={name}").json()
-    if url.startswith([):
+    if url.startswith('['):
         for i in url:
             i["media"] = medias
         video = medias
