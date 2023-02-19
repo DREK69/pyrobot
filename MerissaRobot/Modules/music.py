0506c2cb9efc74e2c4b5e8cd7b3e48c105b3c023
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-02-19 09:10:31 +0530

    Update music.py

--- MerissaRobot/Modules/music.py
+++ MerissaRobot/Modules/music.py
@@ -12,7 +12,6 @@ from MerissaRobot import pbot as Client
 
 @Client.on_message(filters.command(["music", "ytdl", "song"]))
 def song(client, message):
-    global chat_id
     chat_id = message.chat.id
     user_id = message.from_user.id
     user_name = message.from_user.first_name
@@ -51,7 +50,7 @@ def song(client, message):
                 [
                     InlineKeyboardButton(
                         "🔊 Audio",
-                        callback_data=f"audio {videoid}|{thumb}",
+                        callback_data=f"audio {videoid}",
                     ),
                     InlineKeyboardButton("🎥 360p", callback_data=f"360p {videoid}"),
                 ],
@@ -69,7 +68,7 @@ async def callback_query(Client, CallbackQuery):
     ## Download audio
     callback_data = CallbackQuery.data.strip()
     callback = callback_data.split(None, 1)[1]
-    videoid, thumb = callback.split("|")
+    chat_id, videoid = callback.split("|")
     link = f"https://m.youtube.com/watch?v={videoid}"
     youtube_audio = YouTube(link)
     audio = youtube_audio.streams.filter(
@@ -84,7 +83,7 @@ async def callback_query(Client, CallbackQuery):
     try:
         await CallbackQuery.edit_message_media(media=med)
     except Exception as error:
-        await Client.send_message(chat_id, f"Something happened!\n<i>{error}</i>")
+        await CallbackQuery.edit_message_text(f"Something happened!\n<i>{error}</i>")
     os.remove(audio_file)
     os.remove(thumb)
 
@@ -92,7 +91,8 @@ async def callback_query(Client, CallbackQuery):
 @Client.on_callback_query(filters.regex(pattern=r"360p"))
 async def callback_query(Client, CallbackQuery):
     callback_data = CallbackQuery.data.strip()
-    videoid = callback_data.split(None, 1)[1]
+    callback = callback_data.split(None, 1)[1]
+    chat_id, videoid = callback.split("|")
     link = f"https://m.youtube.com/watch?v={videoid}"
     youtube_360 = YouTube(link)
     vid_360 = youtube_360.streams.get_lowest_resolution()
@@ -103,24 +103,27 @@ async def callback_query(Client, CallbackQuery):
     CallbackQuery.message.photo.width
     CallbackQuery.message.photo.height
     download_360 = vid_360.download()
+    med = InputMediaVideo(        
+        download_360,
+        width=width,
+        height=height,
+        caption=youtube_360.title,
+        thumb=thumb,
+        supports_streaming=True
+    )
     try:
-        await Client.send_video(
-            chat_id,
-            download_360,
-            caption=youtube_360.title,
-            thumb=thumb,
-        )
+        await CallbackQuery.edit_message_media(media=med)
     except Exception as error:
-        await Client.send_message(chat_id, f"Error occurred!!\n<i>{error}</i>")
+        await CallbackQuery.edit_message_text(f"Error occurred!!\n<i>{error}</i>")
     os.remove(download_360)
     os.remove(thumb)
-    await m.delete()
 
 
 @Client.on_callback_query(filters.regex(pattern=r"720p"))
 async def callback_query(Client, CallbackQuery):
     callback_data = CallbackQuery.data.strip()
-    videoid = callback_data.split(None, 1)[1]
+    callback = callback_data.split(None, 1)[1]
+    chat_id, videoid = callback.split("|")
     link = f"https://m.youtube.com/watch?v={videoid}"
     youtube_720 = YouTube(link)
     vid_720 = youtube_720.streams.get_by_resolution("720p")
@@ -128,18 +131,21 @@ async def callback_query(Client, CallbackQuery):
         "Downloading And Uploading Started\n\nDownload And Upload Speed could be slow. Please hold on.."
     )
     thumb = await CallbackQuery.message.download()
-    CallbackQuery.message.photo.width
-    CallbackQuery.message.photo.height
+    width = CallbackQuery.message.photo.width
+    height = CallbackQuery.message.photo.height
     download_720 = vid_720.download()
+    med = InputMediaVideo(        
+        download_720,
+        width=width,
+        height=height,
+        caption=youtube_720.title,
+        thumb=thumb,
+        supports_streaming=True
+    )
     try:
-        await Client.send_video(
-            chat_id,
-            download_720,
-            caption=youtube_720.title,
-            thumb=thumb,
-        )
+        await CallbackQuery.edit_message_media(media=med)
     except Exception as error:
-        await Client.send_message(chat_id, f"Error occurred!!\n<i>{error}</i>")
+        await CallbackQuery.edit_message_text(f"Error occurred!!\n<i>{error}</i>")
     os.remove(download_720)
     os.remove(thumb)
-    await m.delete()
+
