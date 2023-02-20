fdfd02df867e05941cdb63b931ddd9f1cdec2914
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-02-20 19:07:37 +0530

    Update insta.py

--- MerissaRobot/Modules/insta.py
+++ MerissaRobot/Modules/insta.py
@@ -14,17 +14,14 @@ async def instadown(_, message):
     posts = get(
         f"https://api.princexd.tech/igdown?apikey=22a34ac86fmsh648c15a7abb6555p1cb539jsn4b193ae50c9f&link={link}"
     ).json()["media"]
-    if isinstance(posts, str):
-        if str(posts.split("https://")[1]).startswith("video"):
-            await message.reply_video(posts, caption=f"Powered By @MerissaRobot")
-        else:
-            await message.reply_photo(posts, caption=f"Powered By @MerissaRobot")
+    if isinstance(posts, str):        
+        await message.reply_document(posts, caption="Powered By @MerissaRobot")
     else:
         mg = []
         for post in posts:
             if str(post.split("https://")[1]).startswith("video"):
-                mg.append(InputMediaVideo(post, caption=f"Powered By @MerissaRobot"))
+                mg.append(InputMediaVideo(post, caption="Powered By @MerissaRobot"))
             else:
-                mg.append(InputMediaPhoto(post, caption=f"Powered By @MerissaRobot"))
+                mg.append(InputMediaPhoto(post, caption="Powered By @MerissaRobot"))
         await message.reply_media_group(mg)
     await msg.delete()
