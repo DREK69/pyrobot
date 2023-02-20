b77b44ccadf0b7cef6ba2a4e744a6cf3e8f3a52a
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-02-20 13:39:06 +0530

    Update insta.py

--- MerissaRobot/Modules/insta.py
+++ MerissaRobot/Modules/insta.py
@@ -9,9 +9,11 @@ instaregex = r"^https:\/\/(instagram\.com|www\.instagram\.com)\/(p|tv|reel|stori
 @pbot.on_message(filters.regex(instaregex))
 async def instadown(_, message):
     name = message.text
-    msg = await message.reply_text("Please Wait Video Uploading...")
-    url = get(f"https://api.princexd.tech/igdown?link={name}").json()
-    vid = url["media"]
-    for video in vid:
-        await message.reply_document(document=video, caption="Powered by @MerissaRobot")
+    msg = await message.reply_text("Processing...")
+    posts = get(f"https://api.princexd.tech/igdown?link={name}").json()["media"]
+    if isinstance(posts, str):
+        await message.reply_document(posts, caption="Powered by @MerissaRobot")
+    else:
+        for post in posts:
+            await message.reply_document(post, caption="Powered by @MerissaRobot")
     await msg.delete()
