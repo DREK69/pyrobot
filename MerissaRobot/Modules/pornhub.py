45eb702ec0b5629c5e61373f12293b7fdb6a8901
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-02-21 09:28:16 +0530

    Update pornhub.py

--- MerissaRobot/Modules/pornhub.py
+++ MerissaRobot/Modules/pornhub.py
@@ -1,22 +1,26 @@
-import asyncio
 import os
-import threading
-
+import asyncio
 import youtube_dl
+import threading
+from pyrogram.errors import MessageNotModified, FloodWait
+ 
+
 from pornhub_api import PornhubApi
 from pornhub_api.backends.aiohttp import AioHttpBackend
-from pyrogram import Client, filters
-from pyrogram.errors import ChatAdminRequired, ChatWriteForbidden, UserNotParticipant
+from youtube_dl.utils import DownloadError
+
+from pyrogram import filters
 from pyrogram.types import (
-    CallbackQuery,
+    Message, InlineQuery,
     InlineKeyboardButton,
     InlineKeyboardMarkup,
-    InlineQuery,
+    CallbackQuery,
     InlineQueryResultArticle,
     InputTextMessageContent,
-    Message,
 )
-from youtube_dl.utils import DownloadError
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+
+from MerissaRobot import pbot as Client
 
 if os.path.exists("downloads"):
     print("✅ File is exist")
@@ -27,20 +31,38 @@ else:
 active = []
 queues = []
 
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
+def edit_msg(client, message, to_edit):
+    try:
+        client.loop.create_task(message.edit(to_edit))
+    except FloodWait as e:
+        client.loop.create_task(asyncio.sleep(e.value))
+    except MessageNotModified:
+        pass
+    except TypeError:
+        pass
 
 def download_progress_hook(d, message, client):
-    if d["status"] == "downloading":
-        current = d.get("_downloaded_bytes_str") or humanbytes(
-            int(d.get("downloaded_bytes", 1))
-        )
+    if d['status'] == 'downloading':
+        current = d.get("_downloaded_bytes_str") or humanbytes(int(d.get("downloaded_bytes", 1)))
         total = d.get("_total_bytes_str") or d.get("_total_bytes_estimate_str")
         file_name = d.get("filename")
-        eta = d.get("_eta_str", "N/A")
+        eta = d.get('_eta_str', "N/A")
         percent = d.get("_percent_str", "N/A")
         speed = d.get("_speed_str", "N/A")
         to_edit = f"📥 <b>Downloading!</b>\n\n<b>Name :</b> <code>{file_name}</code>\n<b>Size :</b> <code>{total}</code>\n<b>Speed :</b> <code>{speed}</code>\n<b>ETA :</b> <code>{eta}</code>\n\n<b>Percentage: </b> <code>{current}</code> from <code>{total} (__{percent}__)</code>"
         threading.Thread(target=edit_msg, args=(client, message, to_edit)).start()
-
 
 async def run_async(func, *args, **kwargs):
     loop = asyncio.get_running_loop()
@@ -53,43 +75,7 @@ def url(filter, client, update):
     else:
         return False
 
-
 url_filter = filters.create(url, name="url_filter")
-
-
-@Client.on_message(filters.incoming & filters.private, group=-1)
-@Client.on_edited_message(filters.incoming & filters.private, group=-1)
-async def subscribe_channel(c: Client, u: Message):
-    if not sub_chat:
-        return
-    try:
-        try:
-            await c.get_chat_member(sub_chat, u.from_user.id)
-        except UserNotParticipant:
-            if sub_chat.isalpha():
-                url = "https://t.me/" + sub_chat
-            else:
-                chat_info = await c.get_chat(sub_chat)
-                url = chat_info.invite_link
-            try:
-                await u.reply_text(
-                    f"Hi {u.from_user.first_name}!\n\nYou must join the redirected channel in order to use this bot, if you've done it, please restart this bot!\n\nUse » /restart",
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("• Join Channel •", url=url),
-                            ],
-                        ],
-                    ),
-                )
-                await u.stop_propagation()
-            except ChatWriteForbidden:
-                pass
-    except ChatAdminRequired:
-        c.send_message(
-            log_chat,
-            "Can't manage the provided channel, make sure I'm the admin on the channel!",
-        )
 
 
 @Client.on_inline_query()
@@ -100,7 +86,7 @@ async def inline_search(c: Client, q: InlineQuery):
     results = []
     try:
         src = await api.search.search(query)
-    except ValueError:
+    except ValueError as e:
         results.append(
             InlineQueryResultArticle(
                 title="I can't found it!",
@@ -118,10 +104,13 @@ async def inline_search(c: Client, q: InlineQuery):
 
         return
 
+
     videos = src.videos
     await backend.close()
+    
 
     for vid in videos:
+
         try:
             pornstars = ", ".join(v for v in vid.pornstars)
             categories = ", ".join(v for v in vid.categories)
@@ -130,15 +119,13 @@ async def inline_search(c: Client, q: InlineQuery):
             pornstars = "N/A"
             categories = "N/A"
             tags = "N/A"
-        capt = (
-            f"Title: `{vid.title}`\n"
-            f"Duration: `{vid.duration}`\n"
-            f"Views: `{vid.views}`\n\n"
-            f"**{pornstars}**\n"
-            f"Category: {categories}\n\n"
-            f"{tags}"
-            f"Link: {vid.url}"
-        )
+        capt = (f"Title: `{vid.title}`\n"
+                f"Duration: `{vid.duration}`\n"
+                f"Views: `{vid.views}`\n\n"
+                f"**{pornstars}**\n"
+                f"Category: {categories}\n\n"
+                f"{tags}"
+                f"Link: {vid.url}")
 
         text = f"{vid.url}"
 
@@ -146,8 +133,7 @@ async def inline_search(c: Client, q: InlineQuery):
             InlineQueryResultArticle(
                 title=vid.title,
                 input_message_content=InputTextMessageContent(
-                    message_text=text,
-                    disable_web_page_preview=True,
+                    message_text=text, disable_web_page_preview=True,
                 ),
                 description=f"Duration: {vid.duration}\nViews: {vid.views}\nRating: {vid.rating}",
                 thumb_url=vid.thumb,
@@ -171,19 +157,16 @@ async def inline_search(c: Client, q: InlineQuery):
 @Client.on_message(url_filter)
 async def options(c: Client, m: Message):
     await m.reply_text(
-        "Tap the button to continue action!",
+        "Tap the button to continue action!", 
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Download",
-                        callback_data=f"d_{m.text}",
+                        "Download", callback_data=f"d_{m.text}",
                     ),
-                ],
-                [
+                ],[
                     InlineKeyboardButton(
-                        "Watch in web",
-                        url=m.text,
+                        "Watch in web", url=m.text,
                     ),
                 ],
             ],
@@ -193,7 +176,7 @@ async def options(c: Client, m: Message):
 
 @Client.on_callback_query(filters.regex("^d"))
 async def get_video(c: Client, q: CallbackQuery):
-    url = q.data.split("_", 1)[1]
+    url = q.data.split("_",1)[1]
     msg = await q.message.edit("Downloading...")
     user_id = q.message.from_user.id
 
@@ -203,7 +186,9 @@ async def get_video(c: Client, q: CallbackQuery):
     else:
         active.append(user_id)
 
-    ydl_opts = {"progress_hooks": [lambda d: download_progress_hook(d, q.message, c)]}
+    ydl_opts = {
+            "progress_hooks": [lambda d: download_progress_hook(d, q.message, c)]
+        }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         try:
@@ -212,7 +197,8 @@ async def get_video(c: Client, q: CallbackQuery):
             await q.message.edit("Sorry, an error occurred")
             return
 
-    for file in os.listdir("."):
+
+    for file in os.listdir('.'):
         if file.endswith(".mp4"):
             await q.message.reply_video(
                 f"{file}",
@@ -223,9 +209,7 @@ async def get_video(c: Client, q: CallbackQuery):
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton(
-                                "• Donate •", url="https://t.me/IamOkayy"
-                            ),
+                            InlineKeyboardButton("• Donate •", url="https://t.me/IamOkayy"),
                         ],
                     ],
                 ),
@@ -234,6 +218,7 @@ async def get_video(c: Client, q: CallbackQuery):
             break
         else:
             continue
+
 
     await msg.delete()
     active.remove(user_id)
