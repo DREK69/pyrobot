d37db8691cb58f07fb713ba38d1c8362e1a868c8
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-02-21 09:05:41 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Modules/pornhub.py
+++ MerissaRobot/Modules/pornhub.py
@@ -21,6 +21,7 @@ async def run_async(func, *args, **kwargs):
     loop = asyncio.get_running_loop()
     return await loop.run_in_executor(None, func, *args, **kwargs)
 
+
 ph = r"^https:\/\/(pornhub\.com|www\.pornhub\.com)"
 
 
