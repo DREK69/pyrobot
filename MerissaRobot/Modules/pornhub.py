e07d209a40a9a337084afd16e7264b1c1b528e60
Prince Prajapati <72757203+MaybePrince@users.noreply.github.com>
2023-02-22 09:06:03 +0530

    Update pornhub.py

--- MerissaRobot/Modules/pornhub.py
+++ MerissaRobot/Modules/pornhub.py
@@ -52,8 +52,7 @@ async def get_video(c: Client, q: CallbackQuery):
     url = q.data.split("_", 1)[1]
     message = await q.message.edit(
         "Downloading Started\n\nDownloading Speed could be Slow Plase wait..."
-    )
-    name = q.message.from_user.first_name
+    )    
     user_id = q.message.from_user.id
     if "some" in active:
         await q.message.edit("Sorry, you can only download videos at a time!")
@@ -92,7 +91,7 @@ async def get_video(c: Client, q: CallbackQuery):
                 thumb=thumb,
                 width=1280,
                 height=720,
-                caption=f"{name}'s content requested has been successfully downloaded!",
+                caption="The content you requested has been successfully downloaded!",
             )
             os.remove(f"{file}")
             break
