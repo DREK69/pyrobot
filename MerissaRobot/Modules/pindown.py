9b033580b5069aa7622be99a612138cd18863e30
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-02-27 12:52:35 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Modules/pindown.py
+++ MerissaRobot/Modules/pindown.py
@@ -3,21 +3,21 @@ from requests import get
 
 from MerissaRobot import pbot
 
-pinregex = (
-    r"^https:\/\/(pin\.it|www\.pinterest\.com|pinterest\.com)"
-)
+pinregex = r"^https:\/\/(pin\.it|www\.pinterest\.com|pinterest\.com)"
 
 
 @pbot.on_message(filters.regex(pinregex))
 async def pindown(_, message):
     link = message.text
     m = await message.reply_text("Processing...")
-    if link == "pinterest.com":                
+    if link == "pinterest.com":
         pin_id = link.split("/", 4)[4]
-        pinvid = get(f"https://api.princexd.tech/pin2?link=https://pin.it/{pin_id}").json()["media"]
+        pinvid = get(
+            f"https://api.princexd.tech/pin2?link=https://pin.it/{pin_id}"
+        ).json()["media"]
         await message.reply_video(pinvid, caption="Powered By @MerissaRobot")
         await m.delete()
-    else:            
+    else:
         pinvid = get(f"https://api.princexd.tech/pin2?link={link}").json()["media"]
         await message.reply_video(pinvid, caption="Powered By @MerissaRobot")
         await m.delete()
