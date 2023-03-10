a93d6d3b4a25547dd6d94b473adb286a844e2b6b
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-03-10 23:23:00 +0530

    Update insta.py

--- MerissaRobot/Modules/insta.py
+++ MerissaRobot/Modules/insta.py
@@ -10,9 +10,9 @@ instaregex = r"^https:\/\/(instagram\.com|www\.instagram\.com)\/(p|tv|reel|stori
 
 @pbot.on_message(filters.regex(instaregex) & filters.private)
 async def instadown(_, message):
-    name = message.text
+    link = message.text
     msg = await message.reply_text("Processing...")
-    posts = get(f"https://api.princexd.tech/igdown?link={name}").json()["media"]
+    posts = get(f"https://api.princexd.tech/igdown?apikey=22a34ac86fmsh648c15a7abb6555p1cb539jsn4b193ae50c9f&link={link}").json()["media"]
     if isinstance(posts, str):
         if ".mp4" in posts:
             await message.reply_video(posts, caption=f"Powered By @MerissaRobot")
