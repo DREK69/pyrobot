bb342220ec2209058c91037bcd74924421f0d0f6
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-03-10 17:44:23 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Modules/insta.py
+++ MerissaRobot/Modules/insta.py
@@ -6,23 +6,24 @@ from MerissaRobot import pbot
 
 instaregex = r"^https:\/\/(instagram\.com|www\.instagram\.com)\/(p|tv|reel|stories)\/([A-Za-z0-9\-_]*)"
 
+
 @pbot.on_message(filters.regex(instaregex) & filters.private)
 async def instadown(_, message):
     name = message.text
     msg = await message.reply_text("Processing...")
     posts = get(f"https://api.princexd.tech/igdown?link={name}").json()["media"]
     if isinstance(posts, str):
-        if '.mp4' in posts:
-            await message.reply_video(posts, caption=f'Powered By @MerissaRobot')
+        if ".mp4" in posts:
+            await message.reply_video(posts, caption=f"Powered By @MerissaRobot")
         else:
-            await message.reply_photo(posts, caption=f'Powered By @MerissaRobot')
+            await message.reply_photo(posts, caption=f"Powered By @MerissaRobot")
     else:
         mg = []
         for post in posts:
-            if str(post.split('https://')[1]).startswith('video'):
-                mg.append(InputMediaVideo(post, caption=f'Powered By @MerissaRobot'))
+            if str(post.split("https://")[1]).startswith("video"):
+                mg.append(InputMediaVideo(post, caption=f"Powered By @MerissaRobot"))
             else:
-                mg.append(InputMediaPhoto(post, caption=f'Powered By @MerissaRobot'))
+                mg.append(InputMediaPhoto(post, caption=f"Powered By @MerissaRobot"))
         await message.reply_media_group(mg)
     await msg.delete()
 
