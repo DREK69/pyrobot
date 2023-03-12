78d9f240c0d4f79fb48d2cc2ec07db910a6a4c49
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-03-12 12:41:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "5294959338:AAGH4OEXd2ErzWtlzknAe_Yl3jO4hR1_3_4")
+TOKEN = os.environ.get("TOKEN", "5294959338:AAEOF0IQ8VLOkxXQ1rYsLEmrpJZSMYQcN64")
 INFOPIC = bool(os.environ.get("INFOPIC", True))
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "MerissaRobot")
 EVENT_LOGS = os.environ.get("EVENT_LOGS", "-1001325865052")
