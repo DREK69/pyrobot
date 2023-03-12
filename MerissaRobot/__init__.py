ae79762535eb5770f6d3e8b61ee8a5a8460bbf15
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-03-12 16:23:22 +0530

    Update __init__.py

--- MerissaRobot/__init__.py
+++ MerissaRobot/__init__.py
@@ -40,7 +40,7 @@ logging.getLogger("apscheduler").setLevel(logging.ERROR)
 logging.getLogger("telethon").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("ptbcontrib.postgres_persistence.postgrespersistence").setLevel(
-    logging.WARNING
+    logging.ERROR
 )
 
 # if version < 3.6, stop bot.
