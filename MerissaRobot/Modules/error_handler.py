c3978f9746366597a2cfdc40c1153e260b7a145b
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-03-18 19:08:51 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Modules/error_handler.py
+++ MerissaRobot/Modules/error_handler.py
@@ -85,7 +85,9 @@ def error_callback(update: Update, context: CallbackContext):
     context.bot.send_message(
         OWNER_ID,
         text=f"#{context.error.identifier}\n<b>An unknown error occured:</b>\n<code>{e}</code>",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Spacebin", url=url)]]),
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Spacebin", url=url)]]
+        ),
         parse_mode="html",
     )
 
