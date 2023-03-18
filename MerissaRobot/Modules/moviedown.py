b65a24c7dc1e0d418a4d1912e79d0157f0c1adf3
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-03-18 17:06:18 +0530

    Update moviedown.py

--- MerissaRobot/Modules/moviedown.py
+++ MerissaRobot/Modules/moviedown.py
@@ -183,7 +183,7 @@ def anime_result(update, context) -> None:
         )
 
 
-dispatcher.add_handler(CommandHandler("movie", find_movie))
+dispatcher.add_handler(CommandHandler("moviedl", find_movie))
 dispatcher.add_handler(CallbackQueryHandler(movie_result, pattern=r"m_"))
 dispatcher.add_handler(CommandHandler("animedl", find_anime))
 dispatcher.add_handler(CallbackQueryHandler(anime_result, pattern=r"a_"))
