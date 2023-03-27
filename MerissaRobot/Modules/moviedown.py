ea157ad3ec516c918e3864562b5722ef1e34f103
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-03-27 10:15:21 +0530

    Update moviedown.py

--- MerissaRobot/Modules/moviedown.py
+++ MerissaRobot/Modules/moviedown.py
@@ -53,7 +53,7 @@ def get_movie(query):
 @pbot.on_message(filters.command("moviedl"))
 def find_movie(_, message):
     if len(message.command) < 2:
-        return await message.reply_text(
+        return message.reply_text(
             "Give some Movie/Series name to Find it on my Database\n\nEx. /moviedl pathaan"
         )
     search_results = message.reply_text("Processing...")
@@ -145,7 +145,7 @@ def get_anime(query):
 @pbot.on_message(filters.command("animedl"))
 def find_anime(_, message):
     if len(message.command) < 2:
-        return await message.reply_text(
+        return message.reply_text(
             "Give some Movie/Series name to Find it on my Database\n\nEx. /animedl naruto"
         )
     search_results = message.reply_text("Processing...")
