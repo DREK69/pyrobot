99d2fadfa3e98324d7091af0e2b027cc0e47a0fe
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-04-02 13:49:05 +0530

    Update chatgpt.py

--- MerissaRobot/Modules/chatgpt.py
+++ MerissaRobot/Modules/chatgpt.py
@@ -14,19 +14,19 @@ active_chats_bot = []
 async def chat_bot_toggle(db, message: Message):
     status = message.text.split(None, 1)[1].lower()
     chat_id = message.chat.id
-    if status == "enable":
+    if status == "on":
         if chat_id not in db:
             db.append(chat_id)
             text = "Chatbot Enabled!"
             return await eor(message, text=text)
         await eor(message, text="ChatBot Is Already Enabled.")
-    elif status == "disable":
+    elif status == "off":
         if chat_id in db:
             db.remove(chat_id)
             return await eor(message, text="Chatbot Disabled!")
         await eor(message, text="ChatBot Is Already Disabled.")
     else:
-        await eor(message, text="**Usage:**\n/chatbot [ENABLE|DISABLE]")
+        await eor(message, text="**Usage:**\n/chatgpt [on|off]")
 
 
 # Enabled | Disable Chatbot
@@ -36,7 +36,7 @@ async def chat_bot_toggle(db, message: Message):
 @capture_err
 async def chatbot_status(_, message: Message):
     if len(message.command) != 2:
-        return await message.ra(message, text="**Usage:**\n/chatgpt [ENABLE|DISABLE]")
+        return await message.ra(message, text="**Usage:**\n/chatgpt [on|off]")
     await chat_bot_toggle(active_chats_bot, message)
 
 
@@ -48,10 +48,7 @@ async def type_and_send(message: Message):
     response = requests.get(f"https://api.princexd.tech/ask?text={query}").json()[
         "answer"
     ]
-    await message.reply_text(response)
-    await message._client.send_chat_action(
-        chat_id,
-    )
+    await message.reply_text(response)   
 
 
 @app.on_message(
