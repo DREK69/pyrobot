3d7cecc1f58f7e953ad89544f6a96fdb56a23991
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-04-08 23:20:32 +0530

    Create pyrocallbacks.py

---
+++ MerissaRobot/Modules/pyrocallbacks.py
@@ -0,0 +1,41 @@
+import requests 
+
+from pyrogram.types import *
+from pyrogram import filters
+from MerissaRobot import pbot as bot
+
+
+
+@bot.on_callback_query(filters.regex("boob"))
+async def boobsc(_, query: CallbackQuery):
+    res = requests.get("https://api.princexd.tech/boobs").json()
+    await query.edit_message_media(
+        InputMediaPhoto(res, caption="Powered By @MerissaRobot"),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Change 🔂",
+                        callback_data="boob",
+                    ),
+                ],
+            ],
+        ),
+    )
+
+@bot.on_callback_query(filters.regex("animepfp"))
+async def animepfpc(_, query: CallbackQuery):
+    res = requests.get("https://api.princexd.tech/animepfp").json()["url"]
+    await query.edit_message_media(
+        InputMediaPhoto(res, caption="Powered By @MerissaRobot"),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Change 🔂",
+                        callback_data="animepfp",
+                    ),
+                ],
+            ],
+        ),
+    )
