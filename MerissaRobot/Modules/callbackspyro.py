70bb1984e7ec72a72f455c5c20f28bd73f5aa943
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-04-08 23:23:53 +0530

    Update callbackspyro.py

--- MerissaRobot/Modules/callbackspyro.py
+++ MerissaRobot/Modules/callbackspyro.py
@@ -39,3 +39,69 @@ async def animepfpc(_, query: CallbackQuery):
             ],
         ),
     )
+
+@bot.on_callback_query(filters.regex("ameme"))
+async def ameme(_, query: CallbackQuery):
+    res = requests.get("https://api.princexd.tech/reddit?query=Animememe").json()[
+        "data"
+    ]
+    url = res["image_url"]
+    text = res["title"]
+    link = res["post_url"]
+    await query.edit_message_media(
+        InputMediaPhoto(url, caption=f"[{text}]({link})"),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Change 🔂",
+                        callback_data="ameme",
+                    ),
+                ],
+            ],
+        ),
+    )
+
+
+@bot.on_callback_query(filters.regex("bmeme"))
+async def memess(_, query: CallbackQuery):
+    res = requests.get("https://api.princexd.tech/meme").json()
+    url = res["image"]
+    text = res["title"]
+    link = res["postLink"]
+    await query.edit_message_media(
+        InputMediaPhoto(url, caption=f"[{text}]({link})"),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Change 🔂",
+                        callback_data="bmeme",
+                    ),
+                ],
+            ],
+        ),
+    )
+
+
+@bot.on_callback_query(filters.regex("cmeme"))
+async def hmeme(_, query: CallbackQuery):
+    res = requests.get("https://meme-api.herokuapp.com/gimme/hentaimemes").json()
+    url = res["url"]
+    text = res["title"]
+    link = res["postLink"]
+    await query.edit_message_media(
+        InputMediaPhoto(url, caption=f"[{text}]({link})"),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Change 🔂",
+                        callback_data="cmeme",
+                    ),
+                ],
+            ],
+        ),
+    )
+
+
