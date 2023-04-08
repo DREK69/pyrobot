a25b12dc9fdb550382595eaf55303e1759ea7fb3
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-04-08 23:22:52 +0530

    Update meme.py

--- MerissaRobot/Modules/meme.py
+++ MerissaRobot/Modules/meme.py
@@ -29,29 +29,6 @@ async def animememes(_, m):
     )
 
 
-@bot.on_callback_query(filters.regex("ameme"))
-async def ameme(_, query: CallbackQuery):
-    res = requests.get("https://api.princexd.tech/reddit?query=Animememe").json()[
-        "data"
-    ]
-    url = res["image_url"]
-    text = res["title"]
-    link = res["post_url"]
-    await query.edit_message_media(
-        InputMediaPhoto(url, caption=f"[{text}]({link})"),
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="Change 🔂",
-                        callback_data="ameme",
-                    ),
-                ],
-            ],
-        ),
-    )
-
-
 @bot.on_message(filters.command("meme"))
 async def memes(_, m):
     res = requests.get("https://api.princexd.tech/meme").json()
@@ -73,28 +50,6 @@ async def memes(_, m):
         ),
     )
 
-
-@bot.on_callback_query(filters.regex("bmeme"))
-async def memess(_, query: CallbackQuery):
-    res = requests.get("https://api.princexd.tech/meme").json()
-    url = res["image"]
-    text = res["title"]
-    link = res["postLink"]
-    await query.edit_message_media(
-        InputMediaPhoto(url, caption=f"[{text}]({link})"),
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="Change 🔂",
-                        callback_data="bmeme",
-                    ),
-                ],
-            ],
-        ),
-    )
-
-
 @bot.on_message(filters.command(["hmeme", "hentaimeme"]))
 async def hetaimemes(_, m):
     res = requests.get("https://meme-api.herokuapp.com/gimme/hentaimemes").json()
@@ -104,27 +59,6 @@ async def hetaimemes(_, m):
     await m.reply_photo(
         url,
         caption=f"[{text}]({link})",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="Change 🔂",
-                        callback_data="cmeme",
-                    ),
-                ],
-            ],
-        ),
-    )
-
-
-@bot.on_callback_query(filters.regex("cmeme"))
-async def hmeme(_, query: CallbackQuery):
-    res = requests.get("https://meme-api.herokuapp.com/gimme/hentaimemes").json()
-    url = res["url"]
-    text = res["title"]
-    link = res["postLink"]
-    await query.edit_message_media(
-        InputMediaPhoto(url, caption=f"[{text}]({link})"),
         reply_markup=InlineKeyboardMarkup(
             [
                 [
