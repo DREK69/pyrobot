e0108c95a97e13fd9e09220341077e9662da13c5
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-06-26 20:41:52 +0530

    Update ask.py

--- MerissaRobot/Modules/ask.py
+++ MerissaRobot/Modules/ask.py
@@ -1,5 +1,5 @@
 from pyrogram import filters
-from requests import get
+from requests import get, post
 
 from MerissaRobot import pbot
 
@@ -11,27 +11,11 @@ async def ask(_, message):
         return await message.reply_text(
             "Give me some questions to ask ChatGPT. Example- /ask question"
         )
-    query = (
-        message.text.split(None, 1)[1]
-        if len(message.command) < 3
-        else message.text.split(None, 1)[1].replace(" ", "%20")
-    )
-    x = get(f"https://api.princexd.tech/ask?text={query}").json()["answer"]
-    await m.edit(f"{x}\n\nPowered by @MerissaRobot", disable_web_page_preview=True)
-
-
-@pbot.on_message(filters.command("generate"))
-async def generate(_, message):
-    m = await message.reply_text("Wait a moment looking for your answer..")
-    if len(message.command) < 2:
-        return await message.reply_text(
-            "Give me some questions to ask ChatGPT. Example- /generate cute girl image 4k"
-        )
-    query = (
-        message.text.split(None, 1)[1]
-        if len(message.command) < 3
-        else message.text.split(None, 1)[1].replace(" ", "%20")
-    )
-    x = get(f"https://api.princexd.tech/genimg?query={query}").json()["imgurl"]
-    await message.reply_photo(x, caption="Powered by @MerissaRobot")
-    await m.delete()
+    query = message.text.split(None, 1)[1]
+    json = {
+        "message": query,
+        "chat_mode": "assistant",
+        "dialog_messages": '[{"bot":"Merissa","user":"Prince"}]',
+    }
+    x = post("https://api.safone.me/chatgpt", data=json).json()["message"]
+    await m.edit(x, disable_web_page_preview=True)
