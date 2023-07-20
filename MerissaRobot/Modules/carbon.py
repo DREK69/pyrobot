42cd30c064d63dd2875ddafc1ca2968f22ce6826
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-07-20 15:20:50 +0530

    Update carbon.py

--- MerissaRobot/Modules/carbon.py
+++ MerissaRobot/Modules/carbon.py
@@ -1,63 +1,57 @@
-from io import BytesIO
-from platform import python_version as y
+import requests
 
-from pyrogram import __version__ as z
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram import __version__ as o
-from telethon import __version__ as s
-
-from MerissaRobot import aiohttpsession as aiosession
-from MerissaRobot import pbot
-from MerissaRobot.Utils.Helpers.errors import capture_err
+from MerissaRobot import pbot as app
 
 
-async def make_carbon(code):
-    url = "https://carbonara.vercel.app/api/cook"
-    async with aiosession.post(url, json={"code": code}) as resp:
-        image = BytesIO(await resp.read())
-    image.name = "carbon.png"
-    return image
+@app.on_message(filters.command(["carbon"]))
+async def carbon(client, message):
+    if message.reply_to_message and message.reply_to_message.text:
+        txt = message.reply_to_message.text
+    elif len(message.command) > 1:
+        txt = message.text.split(None, 1)[1]
+    else:
+        return await message.reply(
+            "Please reply to message or write after command to use write CMD."
+        )
+    nan = await message.reply_text("Processing...")
+    try:
+        data = {
+            "code": txt,
+            "bgcolor": "white"
+        }
+        file = requests.post("https://api.princexd.tech/carbon", json=data).json()["url"]
+        await message.reply_photo(
+            photo=file, caption=f"<b>Carbonimg By :</b> {client.me.mention}"
+        )
+        await nan.delete()
+    except Exception as e:
+        return await message.reply(e)
 
-
-@pbot.on_message(filters.command("carbon"))
-@capture_err
-async def carbon_func(_, message):
-    if not message.reply_to_message:
-        return await message.reply_text("`Reply to a text message to make carbon.`")
-    if not message.reply_to_message.text:
-        return await message.reply_text("`Reply to a text message to make carbon.`")
-    m = await message.reply_text("`Preparing Carbon`")
-    carbon = await make_carbon(message.reply_to_message.text)
-    await m.edit("`Uploading`")
-    await pbot.send_photo(message.chat.id, carbon)
-    await m.delete()
-    carbon.close()
-
-
-MEMEK = "https://telegra.ph/file/1acba17236ed11813854d.jpg"
-
-
-@pbot.on_message(filters.command("repo"))
-async def repo(_, message):
-    await message.reply_photo(
-        photo=MEMEK,
-        caption=f"""✨ **Hey I'm Merissa Robot** 
-
-**Owner repo : [Prince](https://t.me/NoobxCoder)**
-**Python Version :** `{y()}`
-**Library Version :** `{o}`
-**Telethon Version :** `{s}`
-**Pyrogram Version :** `{z}`
-
-**Create your own with click button bellow.**
-""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("Source", callback_data="merissa_source"),
-                    InlineKeyboardButton("Support", url="https://t.me/MerissaxSupport"),
-                ]
-            ]
-        ),
-    )
+@app.on_message(filters.command(["rayso"]))
+async def carbon(client, message):
+    if message.reply_to_message and message.reply_to_message.text:
+        txt = message.reply_to_message.text
+        if len(message.command) > 1:
+            title = message.text.split(None, 1)[1]
+        else:
+            title = message.from_user.username
+    else:
+        return await message.reply(
+            "Please reply to message or write after command to use write CMD."
+        )
+    nan = await message.reply_text("Processing...")
+    try:
+        data = {
+           "code": txt,
+           "title": title,
+           "theme": "breeze",
+           "darkMode": true
+        }
+        file = requests.post("https://api.princexd.tech/rayso", json=data).json()["url"]
+        await message.reply_photo(
+            photo=file, caption=f"<b>Raysoimg By :</b> {client.me.mention}"
+        )
+        await nan.delete()
+    except Exception as e:
+        return await message.reply(e)
