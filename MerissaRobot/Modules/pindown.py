1d6a70d02fd2c83c72f4fb7f1aeed8a716524b95
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-07-22 22:47:02 +0530

    Update pindown.py

--- MerissaRobot/Modules/pindown.py
+++ MerissaRobot/Modules/pindown.py
@@ -10,14 +10,10 @@ pinregex = r"^https:\/\/(pin\.it|www\.pinterest\.com|pinterest\.com)"
 async def pindown(_, message):
     link = message.text
     m = await message.reply_text("Processing...")
-    if link == "pinterest.com":
-        pin_id = link.split("/", 4)[4]
-        pinvid = get(
-            f"https://api.princexd.tech/pin2?link=https://pin.it/{pin_id}"
-        ).json()["media"]
-        await message.reply_video(pinvid, caption="Powered By @MerissaRobot")
-        await m.delete()
-    else:
-        pinvid = get(f"https://api.princexd.tech/pin2?link={link}").json()["media"]
-        await message.reply_video(pinvid, caption="Powered By @MerissaRobot")
-        await m.delete()
+    pin = get(
+        f"https://api.princexd.tech/pin?link={link}"
+    ).json()
+    file = pin["media"]
+    title = pin['title']
+    await message.reply_document(pinvid, caption=f"{title}\n\nUploaded by @MerissaRobot")
+    await m.delete()
