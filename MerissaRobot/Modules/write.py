3ba78a7774606fcc540e9f40268c42a5bb0b2858
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-08-31 14:30:58 +0530

    Update write.py

--- MerissaRobot/Modules/write.py
+++ MerissaRobot/Modules/write.py
@@ -2,7 +2,7 @@ import requests
 from pyrogram import filters
 
 from MerissaRobot import pbot as app
-
+from PIL import Image, ImageDraw, ImageFilter, ImageFont
 
 @app.on_message(filters.command(["write"]))
 async def handwrite(client, message):
@@ -16,11 +16,19 @@ async def handwrite(client, message):
         )
     nan = await message.reply_text("Processing...")
     try:
-        data = {"text": txt}
-        file = requests.post("https://api.princexd.tech/write", json=data).json()["url"]
+        img = Image.open("./MerissaRobot/Utils/Resources/template.jpg")
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype("./MerissaRobot/Utils/Resources/font/assfont.ttf", 30)
+        x, y = 150, 140
+        lines = text_set(data.text)
+        line_height = font.getsize("hg")[1]
+        for line in lines:
+            draw.text((x, y), line, fill=(1, 22, 55), font=font)
+            y = y + line_height - 5
+        file = "generated.jpg"
+        img.save(file)
         await message.reply_photo(
             photo=file, caption=f"<b>Written By :</b> {client.me.mention}"
         )
-        await nan.delete()
     except Exception as e:
         return await message.reply(e)
