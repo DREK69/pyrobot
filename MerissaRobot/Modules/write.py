34e8761ea66c9ee53913012859a4ff74954eaf47
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-08-31 14:37:21 +0530

    Update write.py

--- MerissaRobot/Modules/write.py
+++ MerissaRobot/Modules/write.py
@@ -3,24 +3,38 @@ from pyrogram import filters
 
 from MerissaRobot import pbot as app
 
+def text_set(text):
+    lines = []
+    if len(text) <= 55:
+        lines.append(text)
+    else:
+        all_lines = text.split("\n")
+        for line in all_lines:
+            if len(line) <= 55:
+                lines.append(line)
+            else:
+                y = len(line) // 55
+                lines.extend(line[((x - 1) * 55) : (x * 55)] for x in range(1, y + 2))
+    return lines[:25]
+
 
 @app.on_message(filters.command(["write"]))
 async def handwrite(client, message):
     if message.reply_to_message and message.reply_to_message.text:
-        message.reply_to_message.text
+        text = message.reply_to_message.text
     elif len(message.command) > 1:
-        message.text.split(None, 1)[1]
+        text = message.text.split(None, 1)[1]
     else:
         return await message.reply(
             "Please reply to message or write after command to use write CMD."
         )
-    await message.reply_text("Processing...")
+    w = await message.reply_text("Processing...")
     try:
         img = Image.open("./MerissaRobot/Utils/Resources/template.jpg")
         draw = ImageDraw.Draw(img)
         font = ImageFont.truetype("./MerissaRobot/Utils/Resources/font/assfont.ttf", 30)
         x, y = 150, 140
-        lines = text_set(data.text)
+        lines = text_set(text)
         line_height = font.getsize("hg")[1]
         for line in lines:
             draw.text((x, y), line, fill=(1, 22, 55), font=font)
@@ -30,5 +44,6 @@ async def handwrite(client, message):
         await message.reply_photo(
             photo=file, caption=f"<b>Written By :</b> {client.me.mention}"
         )
+        await w.delete()
     except Exception as e:
-        return await message.reply(e)
+        return await w.edit(e)
