de3582e3c9cfbc37604f2389a9128f4945255708
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-10-02 21:19:38 +0530

    Update broadcast.py

--- MerissaRobot/Modules/broadcast.py
+++ MerissaRobot/Modules/broadcast.py
@@ -22,54 +22,53 @@ async def broadcast(_, message):
                 "**Usage**:\n/broadcast [MESSAGE] or [Reply to a Message]"
             )
         query = okay[1]
+    if message.command[0] == "/broadcastgroups":
+        to_group = True
+    if message.command[0] == "/broadcastusers":
+        to_user = True
     else:
-        if message.command[0] == "/broadcastgroups":
-            to_group = True
-        if message.command[0] == "/broadcastusers":
-            to_user = True
-        else:
-            to_group = to_user = True
-        sent_group = 0
-        sent_user = 0
-        chats = get_all_chats() or []
-        users = get_all_users()
-        broadcast = await message.reply_text("**Broadcasting Message Started...**")
-        if to_group:
-            for chat in chats:
-                try:
-                    chat_id = int(chat.chat_id)
-                    await pbot.forward_messages(
-                        chat_id, y, x
-                    ) if message.reply_to_message else await pbot.send_message(
-                        chat_id, text=query
-                    )
-                    sent_group += 1
-                except FloodWait as e:
-                    flood_time = int(e.x)
-                    if flood_time > 200:
-                        continue
-                    await asyncio.sleep(flood_time)
-                except Exception:
+        to_group = to_user = True
+    sent_group = 0
+    sent_user = 0
+    chats = get_all_chats() or []
+    users = get_all_users()
+    broadcast = await message.reply_text("**Broadcasting Message Started...**")
+    if to_group:
+        for chat in chats:
+            try:
+                chat_id = int(chat.chat_id)
+                await pbot.forward_messages(
+                    chat_id, y, x
+                ) if message.reply_to_message else await pbot.send_message(
+                    chat_id, text=query
+                )
+                sent_group += 1
+            except FloodWait as e:
+                flood_time = int(e.x)
+                if flood_time > 200:
                     continue
-        if to_user:
-            for user in users:
-                try:
-                    chat_id = int(user.user_id)
-                    await pbot.forward_messages(
-                        chat_id, y, x
-                    ) if message.reply_to_message else await pbot.send_message(
-                        chat_id, text=query
-                    )
-                except FloodWait as e:
-                    flood_time = int(e.x)
-                    if flood_time > 200:
-                        continue
-                    await asyncio.sleep(flood_time)
-                except Exception:
+                await asyncio.sleep(flood_time)
+            except Exception:
+                continue
+    if to_user:
+        for user in users:
+            try:
+                chat_id = int(user.user_id)
+                await pbot.forward_messages(
+                    chat_id, y, x
+                ) if message.reply_to_message else await pbot.send_message(
+                    chat_id, text=query
+                )
+            except FloodWait as e:
+                flood_time = int(e.x)
+                if flood_time > 200:
                     continue
-        try:
-            await broadcast.edit_text(
+                await asyncio.sleep(flood_time)
+            except Exception:
+                continue
+    try:
+        await broadcast.edit_text(
                 f"**Broadcast complete.\nGroups Count: {sent_group}\nUsers Count: {sent_user}**"
-            )
-        except:
-            pass
+        )
+    except:
+        pass
