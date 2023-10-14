995663558764e5c9fa0a385c8842a5e9e1cb5c8c
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-10-14 18:56:26 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Modules/shazam.py
+++ MerissaRobot/Modules/shazam.py
@@ -1,8 +1,8 @@
 import os
 from configparser import ConfigParser
-from pyrogram import Client
-from shazamio import Shazam, exceptions, FactoryArtist, FactoryTrack
-from pyrogram import filters, types
+
+from pyrogram import Client, filters, types
+from shazamio import FactoryArtist, FactoryTrack, Shazam, exceptions
 
 shazam = Shazam()
 
@@ -13,9 +13,9 @@ class bot(Client):
         config = ConfigParser()
         config.read(config_file)
         name = name.lower()
-        plugins = {'root': os.path.join(__package__, 'plugins')}
-        api_id = config.get('pyrogram', 'api_id')
-        api_hash = config.get('pyrogram', 'api_hash')
+        plugins = {"root": os.path.join(__package__, "plugins")}
+        api_id = config.get("pyrogram", "api_id")
+        api_hash = config.get("pyrogram", "api_hash")
         super().__init__(
             name,
             api_id=api_id,
@@ -39,23 +39,25 @@ class bot(Client):
 
     async def related(self, track_id):
         try:
-            return (await shazam.related_tracks(track_id=track_id, limit=50, start_from=2))['tracks']
+            return (
+                await shazam.related_tracks(track_id=track_id, limit=50, start_from=2)
+            )["tracks"]
         except exceptions.FailedDecodeJson:
             return None
-    
+
     async def get_artist(self, query: str):
         artists = await shazam.search_artist(query=query, limit=50)
         hits = []
         try:
-            for artist in artists['artists']['hits']:
+            for artist in artists["artists"]["hits"]:
                 hits.append(FactoryArtist(artist).serializer())
             return hits
         except KeyError:
             return None
-        
+
     async def get_artist_tracks(self, artist_id: int):
         tracks = []
-        tem = (await shazam.artist_top_tracks(artist_id=artist_id, limit=50))['tracks']
+        tem = (await shazam.artist_top_tracks(artist_id=artist_id, limit=50))["tracks"]
         try:
             for track in tem:
                 tracks.append(FactoryTrack(data=track).serializer())
@@ -67,40 +69,28 @@ class bot(Client):
 @bot.on_message(filters.audio | filters.video | filters.voice)
 async def voice_handler(_, message):
     file_size = message.audio or message.video or message.voice
-    if 30641629 < file_size.file_size :
-        await message.reply_text(
-            "**⚠️ Max file size has been reached.**"
-        )
+    if 30641629 < file_size.file_size:
+        await message.reply_text("**⚠️ Max file size has been reached.**")
         return
-    file = await message.download(f'{bot.rnd_id()}.mp3')
-    r = (await bot.recognize(file)).get('track', None)
+    file = await message.download(f"{bot.rnd_id()}.mp3")
+    r = (await bot.recognize(file)).get("track", None)
     os.remove(file)
     if r is None:
-        await message.reply_text(
-            '**⚠️ Cannot recognize the audio**'
-        )
+        await message.reply_text("**⚠️ Cannot recognize the audio**")
         return
     out = f'**Title**: `{r["title"]}`\n'
     out += f'**Artist**: `{r["subtitle"]}`\n'
     buttons = [
-            [
-                types.InlineKeyboardButton(
-                    '🎼 Related Songs',
-                    switch_inline_query_current_chat=f'related {r["key"]}',
-                ),
-                types.InlineKeyboardButton(
-                    '🔗 Share',
-                    url=f'{r["share"]["html"]}'
-                )
-            ],
-            [
-                types.InlineKeyboardButton(
-                    '🎵 Listen',
-                    url=f'{r["url"]}'
-                )
-            ],        
-        ]
-    response = r.get('artists', None)
+        [
+            types.InlineKeyboardButton(
+                "🎼 Related Songs",
+                switch_inline_query_current_chat=f'related {r["key"]}',
+            ),
+            types.InlineKeyboardButton("🔗 Share", url=f'{r["share"]["html"]}'),
+        ],
+        [types.InlineKeyboardButton("🎵 Listen", url=f'{r["url"]}')],
+    ]
+    response = r.get("artists", None)
     if response:
         buttons.append(
             [
@@ -111,7 +101,7 @@ async def voice_handler(_, message):
             ]
         )
     await message.reply_photo(
-        r['images']['coverarthq'],
+        r["images"]["coverarthq"],
         caption=out,
-        reply_markup=types.InlineKeyboardMarkup(buttons)
+        reply_markup=types.InlineKeyboardMarkup(buttons),
     )
