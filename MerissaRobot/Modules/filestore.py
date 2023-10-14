bde797d7c6a5ac2b75fefbf90cf3a034d02f8408
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-10-14 13:56:16 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Modules/filestore.py
+++ MerissaRobot/Modules/filestore.py
@@ -102,7 +102,9 @@ async def __reply(update, copied):
     await asyncio.sleep(0.5)  # Wait do to avoid 5 sec flood ban
 
 
-@pbot.on_message(filters.media & filters.private & filters.media_group & filters.incoming)
+@pbot.on_message(
+    filters.media & filters.private & filters.media_group & filters.incoming
+)
 async def _main_grop(bot, update):
     global media_group_id
 
@@ -119,7 +121,9 @@ async def _main_grop(bot, update):
         return
 
 
-@pbot.on_message(filters.media & filters.private & ~filters.media_group & filters.incoming)
+@pbot.on_message(
+    filters.media & filters.private & ~filters.media_group & filters.incoming
+)
 async def _main(bot, update):
     copied = await update.copy(TRACK_CHANNEL)
     await __reply(update, copied)
