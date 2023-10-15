6c3a4fd9ffe533c6c315832f2f0db60c8b5f8fe5
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-10-15 06:09:17 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Utils/Helpers/stream_route.py
+++ MerissaRobot/Utils/Helpers/stream_route.py
@@ -23,10 +23,12 @@ from MerissaRobot import pbot as StreamBot
 
 routes = web.RouteTableDef()
 
+
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
+
 
 multi_clients = {}
 work_loads = {}
