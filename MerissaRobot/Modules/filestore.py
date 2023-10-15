76d50eede8e9644208a13bd1e1571f70320a72be
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-10-15 16:58:17 +0530

    Update filestore.py

--- MerissaRobot/Modules/filestore.py
+++ MerissaRobot/Modules/filestore.py
@@ -132,10 +132,6 @@ async def _main_grop(bot, update):
 
 @pbot.on_message(filters.command("save") & subscribed)
 async def _main(bot, update):
-    global media_group_id
-
-    if int(media_group_id) != int(update.reply_to_message.media_group_id):
-        return
     copied = await update.reply_to_message.copy(TRACK_CHANNEL)
     log_msg = await update.reply_to_message.forward(chat_id=TRACK_CHANNEL)
 
