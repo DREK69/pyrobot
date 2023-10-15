cb53c69cb178373c6479d835eebe36d8568b18cb
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-10-15 14:03:34 +0530

    Update shazam.py

--- MerissaRobot/Modules/shazam.py
+++ MerissaRobot/Modules/shazam.py
@@ -22,21 +22,30 @@ async def voice_handler(_, message):
     if 30641629 < file_size.file_size:
         await message.reply_text("**⚠️ Max file size has been reached.**")
         return
-    ok = await message.reply_text("Finding song for You Please Wait...")
+    ok = await message.reply_text("Downloading Media...")
     file = await message.reply_to_message.download("merissa.mp3")
+    await ok.edit_text("Recognise your Sended media")
     r = (await recognize(file)).get("track", None)
     os.remove(file)
     if r is None:
         await ok.edit_text("**⚠️ Cannot recognize the audio**")
         return
+    await ok.edit_text("Details Founded of Your Media")
     out = f'**Title**: `{r["title"]}`\n'
     out += f'**Artist**: `{r["subtitle"]}`\n'
+    query = f"{r['title']} - {r['subtitle']}"
+    search = await getreq(f"https://api.princexd.tech/ytmsearch?query={query}")
+    videoid = search["results"][0]["id"]
     buttons = [
         [
             types.InlineKeyboardButton("🔗 Share", url=f'{r["share"]["html"]}'),
             types.InlineKeyboardButton("🎵 Listen", url=f'{r["url"]}'),
         ],
+        [
+            types.InlineKeyboardButton("📥 Download Song", url=f'audio {videoid}'),
+        ]
     ]
+    await ok.delete("Uploading Song Details...")
     await message.reply_photo(
         r["images"]["coverarthq"],
         caption=out,
