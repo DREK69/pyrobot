c4df4f6ec41d1183b2284b914f9d9bdeb54e412f
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-10-17 09:13:41 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Modules/phub.py
+++ MerissaRobot/Modules/phub.py
@@ -22,16 +22,20 @@ queues = []
 
 y = {}
 
+
 def download_progress_hook(d, message, client):
-    if d['status'] == 'downloading':
-        current = d.get("_downloaded_bytes_str") or humanbytes(int(d.get("downloaded_bytes", 1)))
+    if d["status"] == "downloading":
+        current = d.get("_downloaded_bytes_str") or humanbytes(
+            int(d.get("downloaded_bytes", 1))
+        )
         total = d.get("_total_bytes_str") or d.get("_total_bytes_estimate_str")
         file_name = d.get("filename")
-        eta = d.get('_eta_str', "N/A")
+        eta = d.get("_eta_str", "N/A")
         percent = d.get("_percent_str", "N/A")
         speed = d.get("_speed_str", "N/A")
         to_edit = f"📥 <b>Downloading!</b>\n\n<b>Name :</b> <code>{file_name}</code>\n<b>Size :</b> <code>{total}</code>\n<b>Speed :</b> <code>{speed}</code>\n<b>ETA :</b> <code>{eta}</code>\n\n<b>Percentage: </b> <code>{current}</code> from <code>{total} (__{percent}__)</code>"
         threading.Thread(target=edit_msg, args=(client, message, to_edit)).start()
+
 
 async def run_async(func, *args, **kwargs):
     loop = asyncio.get_running_loop()
