6330bd1e51cae1af8101ae8f0893afb88a9c7776
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-10-17 15:02:11 +0530

    Update phub.py

--- MerissaRobot/Modules/phub.py
+++ MerissaRobot/Modules/phub.py
@@ -24,44 +24,6 @@ queues = []
 
 y = {}
 
-
-def humanbytes(size):
-    if not size:
-        return ""
-    power = 2**10
-    raised_to_pow = 0
-    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
-    while size > power:
-        size /= power
-        raised_to_pow += 1
-    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
-
-
-def edit_msg(client, message, to_edit):
-    try:
-        client.loop.create_task(message.edit(to_edit))
-    except FloodWait as e:
-        client.loop.create_task(asyncio.sleep(e.value))
-    except MessageNotModified:
-        pass
-    except TypeError:
-        pass
-
-
-def download_progress_hook(d, message, client):
-    if d["status"] == "downloading":
-        current = d.get("_downloaded_bytes_str") or humanbytes(
-            int(d.get("downloaded_bytes", 1))
-        )
-        total = d.get("_total_bytes_str") or d.get("_total_bytes_estimate_str")
-        file_name = d.get("filename")
-        eta = d.get("_eta_str", "N/A")
-        percent = d.get("_percent_str", "N/A")
-        speed = d.get("_speed_str", "N/A")
-        to_edit = f"📥 <b>Downloading!</b>\n\n<b>Name :</b> <code>{file_name}</code>\n<b>Size :</b> <code>{total}</code>\n<b>Speed :</b> <code>{speed}</code>\n<b>ETA :</b> <code>{eta}</code>\n\n<b>Percentage: </b> <code>{current}</code> from <code>{total} (__{percent}__)</code>"
-        threading.Thread(target=edit_msg, args=(client, message, to_edit)).start()
-
-
 async def run_async(func, *args, **kwargs):
     loop = asyncio.get_running_loop()
     return await loop.run_in_executor(None, func, *args, **kwargs)
@@ -142,7 +104,7 @@ async def get_video(c: Client, q: CallbackQuery):
     else:
         active.append(user_id)
 
-    opts = {"progress_hooks": [lambda d: download_progress_hook(d, q.message, c)]}
+    opts = {}
     with youtube_dl.YoutubeDL(opts) as ydl:
         try:
             await run_async(ydl.download, [url])
