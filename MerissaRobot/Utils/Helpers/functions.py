430a71f95c08f1d1bc08fe1f9953ce226ac09b31
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-10-29 15:06:35 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Utils/Helpers/functions.py
+++ MerissaRobot/Utils/Helpers/functions.py
@@ -21,11 +21,9 @@ from asyncio import gather
 from datetime import datetime, timedelta
 from io import BytesIO
 from math import atan2, cos, radians, sin, sqrt
-from os import execvp
 from random import randint
 from re import findall
 from re import sub as re_sub
-from sys import executable
 
 import aiofiles
 import speedtest
