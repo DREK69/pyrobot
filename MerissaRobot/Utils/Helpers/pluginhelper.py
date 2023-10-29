4295dd94519cfb270bc9f02451d84db37fc30658
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-10-29 20:23:29 +0530

    Update pluginhelper.py

--- MerissaRobot/Utils/Helpers/pluginhelper.py
+++ MerissaRobot/Utils/Helpers/pluginhelper.py
@@ -353,28 +353,3 @@ def capture_err(func):
             raise err
 
     return capture
-
-
-# Special credits to TheHamkerCat
-
-
-async def member_permissions(chat_id, user_id):
-    perms = []
-    member = await kp.get_chat_member(chat_id, user_id)
-    if member.can_post_messages:
-        perms.append("can_post_messages")
-    if member.can_edit_messages:
-        perms.append("can_edit_messages")
-    if member.can_delete_messages:
-        perms.append("can_delete_messages")
-    if member.can_restrict_members:
-        perms.append("can_restrict_members")
-    if member.can_promote_members:
-        perms.append("can_promote_members")
-    if member.can_change_info:
-        perms.append("can_change_info")
-    if member.can_invite_users:
-        perms.append("can_invite_users")
-    if member.can_pin_messages:
-        perms.append("can_pin_messages")
-    return perms
