172816679521be0a12250eea8959075a8b0f0bc6
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-10-30 16:33:44 +0530

    Update rename.py

--- MerissaRobot/Modules/rename.py
+++ MerissaRobot/Modules/rename.py
@@ -6,7 +6,7 @@ from MerissaRobot import pbot as bot
 
 
 @bot.on_message(filters.command("rename"))
-def rename(_, message):
+async def rename(_, message):
     try:
         filename = message.text.replace(message.text.split(" ")[0], "")
 
@@ -17,9 +17,9 @@ def rename(_, message):
     if reply:
         if reply.document.file_size > 10485760:
             return message.reply_text("You can only rename files smaller than 10MB.")
-        x = message.reply_text("📥 Downloading.....")
+        x = await message.reply_text("📥 Downloading.....")
         path = reply.download(file_name=filename)
-        x.edit("📤 Uploading.....")
-        message.reply_document(path)
+        await x.edit("📤 Uploading.....")
+        await message.reply_document(path)
         os.remove(path)
-        x.delete()
+        await x.delete()
