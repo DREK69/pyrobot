776c991671d242abae15a5460a6b4fe66805f370
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-11-06 14:27:08 +0530

    Update telegraph.py

--- MerissaRobot/Modules/telegraph.py
+++ MerissaRobot/Modules/telegraph.py
@@ -4,81 +4,65 @@ from datetime import datetime
 from PIL import Image
 from telegraph import Telegraph, exceptions, upload_file
 
-from MerissaRobot import telethn as tbot
-from MerissaRobot.events import register
+from MerissaRobot import pbot
 
-Anonymous = "MerissaRobot"
 TMP_DOWNLOAD_DIRECTORY = "./"
 telegraph = Telegraph()
-r = telegraph.create_account(short_name=Anonymous)
+r = telegraph.create_account(short_name="MerissaRobot")
 auth_url = r["auth_url"]
 
 
-@register(pattern="^/tg(m|t) ?(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    optional_title = event.pattern_match.group(2)
-    if event.reply_to_msg_id:
-        start = datetime.now()
-        r_message = await event.get_reply_message()
-        input_str = event.pattern_match.group(1)
-        if input_str == "m":
-            downloaded_file_name = await tbot.download_media(
-                r_message, TMP_DOWNLOAD_DIRECTORY
-            )
-            end = datetime.now()
-            ms = (end - start).seconds
-            h = await event.reply(
-                "Downloaded to {} in {} seconds.".format(downloaded_file_name, ms)
-            )
-            if downloaded_file_name.endswith((".webp")):
-                resize_image(downloaded_file_name)
-            try:
-                start = datetime.now()
-                media_urls = upload_file(downloaded_file_name)
-            except exceptions.TelegraphException as exc:
-                await h.edit("ERROR: " + str(exc))
-                os.remove(downloaded_file_name)
-            else:
-                end = datetime.now()
-                (end - start).seconds
-                os.remove(downloaded_file_name)
-                await h.edit(
-                    "Uploaded to https://te.legra.ph{}".format(media_urls[0]),
-                    link_preview=False,
-                )
-        elif input_str == "t":
-            user_object = await tbot.get_entity(r_message.sender_id)
-            title_of_page = user_object.first_name  # + " " + user_object.last_name
-            # apparently, all Users do not have last_name field
-            if optional_title:
-                title_of_page = optional_title
-            page_content = r_message.message
-            if r_message.media:
-                if page_content != "":
-                    title_of_page = page_content
-                downloaded_file_name = await tbot.download_media(
-                    r_message, TMP_DOWNLOAD_DIRECTORY
-                )
-                m_list = None
-                with open(downloaded_file_name, "rb") as fd:
-                    m_list = fd.readlines()
-                for m in m_list:
-                    page_content += m.decode("UTF-8") + "\n"
-                os.remove(downloaded_file_name)
-            page_content = page_content.replace("\n", "<br>")
-            response = telegraph.create_page(title_of_page, html_content=page_content)
-            end = datetime.now()
-            ms = (end - start).seconds
-            await event.reply(
-                "Pasted to https://te.legra.ph/{} in {} seconds.".format(
-                    response["path"], ms
-                ),
-                link_preview=False,
-            )
+@pbot.on_message(filters.command('tgm'))
+async def get_link_group(client, message):
+    try:
+        text = await message.reply("Processing...")
+        async def progress(current, total):
+            await text.edit_text(f"📥 Downloading media... {current * 100 / total:.1f}%")
+        try:
+            location = f"./media/group/"
+            local_path = await message.reply_to_message.download(location, progress=progress)
+            await text.edit_text("📤 Uploading to Telegraph...")
+            upload_path = upload_file(local_path) 
+            await text.edit_text(f"**🌐 | Telegraph Link**:\n\n<code>https://telegra.ph{upload_path[0]}</code>")     
+            os.remove(local_path) 
+        except Exception as e:
+            await text.edit_text(f"**❌ | File upload failed**\n\n<i>**Reason**: {e}</i>")
+            os.remove(local_path) 
+            return         
+    except Exception:
+        pass 
+
+@pbot.on_message(filters.command('tgt'))
+async def get_link_group(client, message):
+    optional_title = message.text.split(None, 1)[1]
+    m = await message.reply_text("Processing")
+    async def progress(current, total):
+        await m.edit_text(f"📥 Downloading media... {current * 100 / total:.1f}%")
+    title_of_page = message.from_user.first_name
+    if optional_title:
+        title_of_page = optional_title
+    page_content = message.reply_to_message
+    if r_message.media:
+        if page_content != "":
+            title_of_page = page_content
+        location = f"./media/group/"
+        downloaded_file_name = await message.reply_to_message.download(location, progress=progress)
+        m_list = None
+        with open(downloaded_file_name, "rb") as fd:
+            m_list = fd.readlines()
+            for m in m_list:
+                page_content += m.decode("UTF-8") + "\n"
+            os.remove(downloaded_file_name)
+        page_content = page_content.replace("\n", "<br>")
+        response = telegraph.create_page(title_of_page, html_content=page_content)
+        end = datetime.now()
+        ms = (end - start).seconds
+        await m.edit_text(
+        "Pasted to https://te.legra.ph/{} in {} seconds.".format(
+                response["path"], ms
+            ))
     else:
-        await event.reply("Reply to a message to get a permanent telegra.ph link.")
+        await message.reply("Reply to a message to get a permanent telegra.ph link.")
 
 
 def resize_image(image):
