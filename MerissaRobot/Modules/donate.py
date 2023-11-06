37fa7457ca45f5615a14b413660cc6a792cc3a84
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-11-06 17:25:10 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Modules/donate.py
+++ MerissaRobot/Modules/donate.py
@@ -11,6 +11,15 @@ async def makeqr(c, m):
     if int(amount.text) < 10:
         return await m.reply_text("Min. Donation amount is 10rs")
     url = f"https://pay.princexd.tech/prajapatiprince@apl/{amount.text}"
-    button = InlineKeyboardMarkup([[InlineKeyboardButton("Pay", url=url), InlineKeyboardButton("Done ✅", callback_data="donated")]])
+    button = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("Pay", url=url),
+                InlineKeyboardButton("Done ✅", callback_data="donated"),
+            ]
+        ]
+    )
     x = f"Merissa UPI Payment - SCAN & PAY 📃 \n\nAmount: ₹{amount.text}\nMethod: UPI\n\nInstructions: Click below 'Pay' button to pay payment, after paying just comeback and click on 'done' button!"
-    await m.reply_text(x, caption=f"<b>Thanks For Your donation</b>", reply_markup=button, quote=True)
+    await m.reply_text(
+        x, caption=f"<b>Thanks For Your donation</b>", reply_markup=button, quote=True
+    )
