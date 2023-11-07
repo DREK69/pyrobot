4fd6e4f48d703fffae5cd3d1b0a4607450a61089
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-11-07 11:34:13 +0530

    Create sangmata.py

---
+++ MerissaRobot/Modules/sangmata.py
@@ -0,0 +1,37 @@
+import asyncio
+
+from pyrogram import filters
+
+from MerissaRobot import pbot
+from MerissaRobot import user as app2
+from MerissaRobot.helpers import subscribe
+
+
+@pbot.on_message(filters.command("sg"))
+@subscribe
+async def sangmata(client, message):
+    if message.reply_to_message:
+        user = message.reply_to_message.from_user.id
+    elif not message.reply_to_message and len(message.command) == 1:
+        user = message.from_user.id
+    elif not message.reply_to_message and len(message.command) != 1:
+        anu = message.text.split(None, 1)[1]
+        iya = await app.get_users(anu)
+        user = iya.id
+    else:
+        return await message.reply("__Reply to message or give Username/UserId__")
+    user_id = user
+    sgbot = await message.reply("**🔍 Searching**")
+    await app2.unblock_user("@sangmata_beta_bot")
+    sang = await app2.send_message("SangMata_beta_bot", f" {user_id}")
+    await sang.delete()
+    await asyncio.sleep(3)
+    async for msg in app2.search_messages("SangMata_beta_bot", limit=4):
+        if "This result is incomplete" or "Link To Profile" in msg.text:
+            await msg.delete()
+        if "No records found" in msg.text:
+            await sgbot.edit("No Data Found")
+            await msg.delete()
+        if "Name" in msg.text:
+            await sgbot.edit(msg.text)
+            await msg.delete()
