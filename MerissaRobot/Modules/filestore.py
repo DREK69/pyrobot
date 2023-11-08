d434d1c66efd5ebc2958bfd2806cfc9bb8941306
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-11-08 13:13:11 +0530

    Update filestore.py

--- MerissaRobot/Modules/filestore.py
+++ MerissaRobot/Modules/filestore.py
@@ -49,7 +49,7 @@ async def _startfile(bot, update):
             [
                 [
                     InlineKeyboardButton(text="🎥 Watch ", url=f"{link}"),
-                    InlineKeyboardButton(text="📥 Download ", url=f"ytdown {videoid}"),
+                    InlineKeyboardButton(text="📥 Download ", callback_data=f"ytdown {videoid}"),
                 ],
                 [
                     InlineKeyboardButton(text="🔄 Close", callback_data="close"),
