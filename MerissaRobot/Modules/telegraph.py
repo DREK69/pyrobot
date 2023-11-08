7d3d15801cd479b580da7b80fd1ed3c6c357de77
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-11-08 23:44:13 +0530

    Update telegraph.py

--- MerissaRobot/Modules/telegraph.py
+++ MerissaRobot/Modules/telegraph.py
@@ -1,91 +1,54 @@
 import os
-
-from PIL import Image
-from pyrogram import filters
-from telegraph import Telegraph, upload_file
+from telegraph import Telegraph, exceptions, upload_file
 
 from MerissaRobot import pbot
+from pyrogram import Client, filters
+from main.helper_func.plugin_helpers import convert_to_image
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-TMP_DOWNLOAD_DIRECTORY = "./"
 telegraph = Telegraph()
-r = telegraph.create_account(short_name="MerissaRobot")
+r = telegraph.create_account(short_name="Mr.Stark")
 auth_url = r["auth_url"]
 
-
-@pbot.on_message(filters.command("tgm"))
-async def get_link_group(client, message):
+@pbot.on_message(filters.command(["telegraph", "tgraph"]))
+async def telegrapher(c, m):
+  tg = await m.reply_text("`Please wi8..`")
+  if not m.reply_to_message:
+    await tg.edit("`This command needs reply to work..`")
+    return
+  if m.reply_to_message.media:
+    if m.reply_to_message.image:
+      m_d = await m.reply_to_message.download()
     try:
-        text = await message.reply_text("Processing...")
-
-        async def progress(current, total):
-            await text.edit_text(f"📥 Downloading media... {current * 100 / total:.1f}%")
-
-        try:
-            location = f"./media/group/"
-            local_path = await message.reply_to_message.download(
-                location, progress=progress
-            )
-            await text.edit_text("📤 Uploading to Telegraph...")
-            upload_path = upload_file(local_path)
-            await text.edit_text(
-                f"**🌐 | Telegraph Link**:\n\n<code>https://telegra.ph{upload_path[0]}</code>"
-            )
-            os.remove(local_path)
-        except Exception as e:
-            await text.edit_text(
-                f"**❌ | File upload failed**\n\n<i>**Reason**: {e}</i>"
-            )
-            os.remove(local_path)
-            return
-    except Exception:
-        pass
-
-
-@pbot.on_message(filters.command("tgt"))
-async def get_link_group(client, message):
-    optional_title = message.text.split(None, 1)[1]
-    m = await message.reply_text("Processing...")
-
-    async def progress(current, total):
-        await m.edit_text(f"📥 Downloading media... {current * 100 / total:.1f}%")
-
-    title_of_page = message.from_user.first_name
-    if optional_title:
-        title_of_page = optional_title
-
-    r_message = message.reply_to_message
-    if r_message:
-        page_content = r_message.text
-        if r_message.media:
-            if page_content != "":
-                title_of_page = page_content
-            location = f"./media/group/"
-            downloaded_file_name = await message.reply_to_message.download(
-                location, progress=progress
-            )
-            m_list = None
-            with open(downloaded_file_name, "rb") as fd:
-                m_list = fd.readlines()
-                for m in m_list:
-                    page_content += m.decode("UTF-8") + "\n"
-                os.remove(downloaded_file_name)
-
-        page_content = page_content.replace("\n", "<br>")
-        response = telegraph.create_page(title_of_page, html_content=page_content)
-        await m.edit_text(f"Pasted to https://te.legra.ph/{response['path']}")
-    else:
-        await message.reply("Reply to a message to get a permanent telegra.ph link.")
-
-
-def resize_image(image):
-    im = Image.open(image)
-    im.save(image, "PNG")
-
-
-__help__ = """
-I can upload files to Telegraph
- ❍ /tgm :Get Telegraph Link Of Replied Media
- ❍ /tgt :Get Telegraph Link of Replied Text
-"""
-
-__mod_name__ = "Telegraph 📋"
+      media_url = upload_file(m_d)
+    except exceptions.TelegraphException as exc:
+      await tg.edit("Telegraph failed\n`{exc}`")
+      os.remove(m_d)
+      return
+    up_done = "__Uploaded to Telegraph__"
+    url = "https://graph.org/" + media_url[0]
+    button = InlineKeyboardButton(text="Click here", url=url)
+    keyboard = InlineKeyboardMarkup([[button]])
+    await m.reply_text(
+      up_done, reply_markup=keyboard
+      )
+    os.remove(m_d)
+    await tg.delete()
+  elif m.reply_to_message.text:
+    try:
+        page_title = m.text.split(None, 1)[1]
+    except IndexError:
+        page_title = m.from_user.first_name
+    page_text = m.reply_to_message.text
+    page_text = page_text.replace("\n", "</br>")
+    try:
+      response = telegraph.create_page(page_title, html_content=page_text)
+      wow_graph = "__Uploaded to Telegraph__"
+      button = InlineKeyboardButton(text="Click here", url=response['url'])
+      keyboard = InlineKeyboardMarkup([[button]])
+      await m.reply_text(
+        wow_graph, reply_markup=keyboard
+        )
+      await tg.delete()
+    except exceptions.TelegraphException as exc:
+      await tg.edit("Telegram Failed\n `{exc}`")
