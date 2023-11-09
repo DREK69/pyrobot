1f8facfd9af526c352f4549e7f8a6b9bc4e49308
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-11-09 10:44:57 +0530

    Update and rename mmf.py to imagetools.py

--- MerissaRobot/Modules/imagetools.py
+++ MerissaRobot/Modules/imagetools.py
@@ -9,6 +9,40 @@ from pyrogram.types import Message
 
 from MerissaRobot import pbot
 
+import os
+from pyrogram import Client, filters
+from telegraph import upload_file
+
+
+@pbot.on_message(filters.command(["wasted"]))
+async def wasted(bot, message):
+    gta = await bot.send_message(message.chat.id, "`Processing...`")
+    if not message.reply_to_message:
+        await gta.edit("`Reply to a photo :(`")
+        return
+    ok = message.reply_to_message
+    pic = await bot.download_media(ok)
+    poto_url = upload_file(pic)
+    imglink = f"https://telegra.ph{poto_url[0]}"
+    url = f"https://some-random-api.com/canvas/wasted?avatar={imglink}"
+    await message.reply_photo(url)
+    await gta.delete()
+    os.remove(pic)
+
+@pbot.on_message(filters.command(["passed"]))
+async def mission_passed(bot, message):
+    gta = await bot.send_message(message.chat.id, "`Processing...`")
+    if not message.reply_to_message:
+        await gta.edit("`Reply to a photo :(`")
+        return
+    ok = message.reply_to_message
+    pic = await bot.download_media(ok)
+    poto_url = upload_file(pic)
+    imglink = f"https://telegra.ph{poto_url[0]}"
+    url = f"https://some-random-api.com/canvas/overlay/passed?avatar={imglink}"
+    await message.reply_photo(url)
+    await gta.delete()
+    os.remove(pic)
 
 async def edit_or_reply(msg: Message, **kwargs):
     func = (
