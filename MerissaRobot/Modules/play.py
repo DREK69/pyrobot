a1a168fc1c5cc4d3dcf6e782c56dec1a78c3b88c
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-11-09 23:46:45 +0530

    Update play.py

--- MerissaRobot/Modules/play.py
+++ MerissaRobot/Modules/play.py
@@ -31,7 +31,7 @@ from MerissaRobot.Utils.Helpers.vcfunction import *
 
 
 @pbot.on_message(
-    filters.command("play") & ~filters.private & ~filters.forwarded & ~filters.via_bot
+    filters.command("play") & filters.group & ~filters.forwarded & ~filters.via_bot
 )
 async def play(_, message: Message):
     merissa = await message.reply_text("Processing Please Wait...")
