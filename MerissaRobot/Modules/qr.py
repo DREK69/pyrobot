d5602def91a8b4f5d9d93de5aa323d088af40848
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-11-09 05:13:07 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Modules/qr.py
+++ MerissaRobot/Modules/qr.py
@@ -1,43 +1,53 @@
 import os
+
 import cv2
 import qrcode
-from pyrogram import Client, filters
+from pyrogram import filters
+
 from MerissaRobot import pbot
 
-@pbot.on_message(filters.command('qr'))
+
+@pbot.on_message(filters.command("qr"))
 async def qr(c, m):
     if " " in m.text:
-        tdl = await m.reply_text("**Plz wi8 Bruh!!**")
+        await m.reply_text("**Plz wi8 Bruh!!**")
         text = str(m.text).split(" ", 1)[1]
-        qr = qrcode.QRCode(version=None,
-                           error_correction=qrcode.constants.ERROR_CORRECT_L,
-                           box_size=10, border=4)
+        qr = qrcode.QRCode(
+            version=None,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
         qr.add_data(text)
         qr.make(fit=True)
         img = qr.make_image(fill_color="black", back_color="white")
-        img.save('qr.png')
+        img.save("qr.png")
         try:
-            await c.send_photo(m.chat.id, 'qr.png')
+            await c.send_photo(m.chat.id, "qr.png")
         except:
-            await c.send_document(m.chat.id, 'qr.png')
-        os.remove('qr.png')
+            await c.send_document(m.chat.id, "qr.png")
+        os.remove("qr.png")
     elif m.reply_to_message.text:
         text = m.reply_to_message.text
-        qr = qrcode.QRCode(version=None,
-                           error_correction=qrcode.constants.ERROR_CORRECT_L,
-                           box_size=10, border=4)
+        qr = qrcode.QRCode(
+            version=None,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
         qr.add_data(text)
         qr.make(fit=True)
         img = qr.make_image(fill_color="black", back_color="white")
-        img.save('qr.png')
+        img.save("qr.png")
         try:
-            await c.send_photo(m.chat.id, 'qr.png')
+            await c.send_photo(m.chat.id, "qr.png")
         except:
-            await c.send_document(m.chat.id, 'qr.png')
-        os.remove('qr.png')
+            await c.send_document(m.chat.id, "qr.png")
+        os.remove("qr.png")
     elif not m.reply_to_message:
         await m.reply(
-            '**Hah! What to do with empty command?\nReply an image to scan or send text along with command to make qr.**')
+            "**Hah! What to do with empty command?\nReply an image to scan or send text along with command to make qr.**"
+        )
     elif m.reply_to_message.photo:
         x = await m.reply_text("Processing...")
         try:
@@ -49,7 +59,7 @@ async def qr(c, m):
             await x.edit("Failed to get data")
         os.remove(qr_code)
     else:
-        await m.reply('Unsupported!')
+        await m.reply("Unsupported!")
 
 
 @app.on_message(filters.command("readqr"))
