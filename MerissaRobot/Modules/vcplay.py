f102b811a0222cc0400433708f4df7756edf1a10
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-11-16 20:04:17 +0530

    Update vcplay.py

--- MerissaRobot/Modules/vcplay.py
+++ MerissaRobot/Modules/vcplay.py
@@ -129,10 +129,11 @@ async def play(_, message):
         if message.reply_to_message
         else None
     )
-    video = (message.reply_to_message.video)
+    video = (
+        (message.reply_to_message.video)
         if message.reply_to_message
         else None
-    (
+    )
     url = get_url(message)
     stream_type = ""
     if message.reply_to_message:
