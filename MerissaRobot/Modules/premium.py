2662d02247187f0585168c56992bc99faa8ee78e
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-11-21 23:23:14 +0530

    Update premium.py

--- MerissaRobot/Modules/premium.py
+++ MerissaRobot/Modules/premium.py
@@ -316,8 +316,7 @@ async def autoapproveg(client, message: ChatJoinRequest):
     await client.send_message(
         chat.id,
         "{} wants to join {}".format(user.first_name, chat.title),
-        reply_markup=keyboard,
-        parse_mode=ParseMode.HTML,
+        reply_markup=keyboard
     )
 
 
