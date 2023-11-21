e861caee5daa20de045b0f5aade21ede72863f8e
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-11-21 12:20:38 +0530

    Update chatbot.py

--- MerissaRobot/Modules/chatbot.py
+++ MerissaRobot/Modules/chatbot.py
@@ -133,7 +133,7 @@ def chatbot(update: Update, context: CallbackContext):
         if not merissa_message(context, message):
             return
         bot.send_chat_action(chat_id, action="typing")
-        url = f"https://kora-api.vercel.app/chatbot/2d94e37d-937f-4d28-9196-bd5552cac68b/MerissaRobot/Prince/message={message.text}"
+        url = f"https://merissachatbot.vercel.app/chatbot/Merissa/Prince/message={message.text}"
         results = requests.get(url).json()
         sleep(0.5)
         message.reply_text(results["reply"])
