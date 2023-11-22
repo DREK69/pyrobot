efb9c22880295d36fa75de0962bb741208f4584a
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-11-22 14:59:25 +0530

    Update ttsnstt.py

--- MerissaRobot/Modules/ttsnstt.py
+++ MerissaRobot/Modules/ttsnstt.py
@@ -6,6 +6,7 @@ from inspect import getfullargspec
 from aiohttp import BasicAuth, ClientSession
 from gtts import gTTS
 from pyrogram import filters
+from pyrogram.types import Message
 
 from MerissaRobot import IBM_WATSON_CRED_PASSWORD, IBM_WATSON_CRED_URL, pbot
 
