3ec49bf1124fb4c456f0a92b807133ab68a568bc
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-11-23 17:18:59 +0530

    Update __init__.py

--- MerissaRobot/__init__.py
+++ MerissaRobot/__init__.py
@@ -50,33 +50,45 @@ getLogger("telegram").setLevel(ERROR)
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 dispatcher = updater.dispatcher
 
-BOT_ID = dispatcher.bot.id
-BOT_USERNAME = dispatcher.bot.username
-BOT_NAME = dispatcher.bot.first_name
-BOT_MENTION = "https://t.me/MerissaRobot"
-ASS_ID = "5249696122"
-ASS_NAME = "Merissa Assistant"
-ASS_USERNAME = "MerissaAssistant"
-ASS_MENTION = "https://t.me/merissaassistant"
-
 pbot = Client(
     "MerissaRobot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=TOKEN,
 )
+BOT_ID = pbot.me.id
+BOT_USERNAME = pbot.me.username
+BOT_NAME = pbot.me.first_name
+BOT_MENTION = pbot.me.mention
+
 user = Client(
     "MerissaMusic",
     api_id=API_ID,
     api_hash=API_HASH,
     session_string=str(STRING_SESSION),
 )
+ASS_ID = user.me.id
+ASS_NAME = user.me.first_name
+ASS_USERNAME = user.me.username
+ASS_MENTION = user.me.mention
+
 pytgcalls = PyTgCalls(user)
 
 telethn = TelegramClient(MemorySession(), API_ID, API_HASH)
 
 DEV_USERS.add(OWNER_ID)
 sw = None
+
+def dirr():
+    for file in os.listdir():
+        if file.endswith(".jpg"):
+            os.remove(file)
+    for file in os.listdir():
+        if file.endswith(".jpeg"):
+            os.remove(file)
+    if "downloads" not in os.listdir():
+        os.mkdir("downloads")
+    LOGGER(__name__).info("Directories Updated.")
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
