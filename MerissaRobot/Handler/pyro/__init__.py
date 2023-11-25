b876a4b56c856498d1ec3b6f9b263761b152caf1
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-11-25 19:35:40 +0530

    Update __init__.py

--- MerissaRobot/Handler/pyro/__init__.py
+++ MerissaRobot/Handler/pyro/__init__.py
@@ -1,1 +1,17 @@
+from pyrogram import Client
 
+from config import *
+
+pbot = Client(
+    "MerissaRobot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=TOKEN,
+)
+
+user = Client(
+    "MerissaMusic",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=str(STRING_SESSION),
+)
