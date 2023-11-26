0c35d482253d7afddb8ea45c94befdc9329cf676
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-11-26 10:30:08 +0000

    Auto Fixes By Merissa

--- MerissaRobot/Handler/pyro/ytmusic.py
+++ MerissaRobot/Handler/pyro/ytmusic.py
@@ -7,6 +7,7 @@ async def ytmusicapi(query):
     result = ytmusic.search(query, filter="songs", limit=10)
     return {"results": result}
 
+
 def ytmusicapi(videoid):
     result = ytmusic.get_song(videoid)
     return {"results": result}
