2569e6aca760313749f4b152580b5620628e8965
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-11-26 16:19:29 +0530

    Update ytmusic.py

--- MerissaRobot/Handler/pyro/ytmusic.py
+++ MerissaRobot/Handler/pyro/ytmusic.py
@@ -1,13 +1,20 @@
+import re
+
 from ytmusicapi import YTMusic
 
 ytmusic = YTMusic()
 
+def getvideoid(link):
+    pattern = re.compile(r'(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})')
+    match = pattern.search(link)    
+    videoid = match.group(1)
+    return videoid
 
-async def ytmsearch(query):
-    result = ytmusic.search(query, filter="songs", limit=10)
-    return {"results": result}
 
-
-def ytmtrack(videoid):
-    result = ytmusic.get_song(videoid)
+def ytmsearch(query):
+    if "https" in query:
+        videoid = getvideoid(link)
+        result = ytmusic.get_song(videoid)
+    else:
+        result = ytmusic.search(query, filter="songs", limit=10)
     return {"results": result}
