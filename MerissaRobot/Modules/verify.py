480fe740b3cb28faaa7fb0305b44e44adfcff8fd
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-11-29 23:45:02 +0530

    Update verify.py

--- MerissaRobot/Modules/verify.py
+++ MerissaRobot/Modules/verify.py
@@ -4,7 +4,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from MerissaRobot import LOGGER, pbot
 
 
-@pbot.on_message(filters.command("verify") & filters.group)
+@pbot.on_message(filters.command("verify") & ~filters.private)
 async def verifylink(bot, update):
     chat = update.chat
     uid = update.from_user.id
