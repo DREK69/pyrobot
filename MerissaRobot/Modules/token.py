1116d7a8bd04f3e146a69fb495a4f16107f9cde6
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-12-05 09:45:16 +0530

    Update token.py

--- MerissaRobot/Modules/token.py
+++ MerissaRobot/Modules/token.py
@@ -6,6 +6,7 @@ from MerissaRobot import ERROR_LOG
 from MerissaRobot import pbot as app
 from MerissaRobot.Database.mongo import leveldb
 from MerissaRobot.helpers import subscribe
+from secrets import token_urlsafe as uls
 
 
 @app.on_message(
@@ -16,19 +17,7 @@ async def token(client, message):
     m = await message.reply_text("Processing")
     toggle = leveldb["myFirstDatabase"]["jsons"]
     user = leveldb["MerissaApi"]["user"]
-    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    chars1 = "1234564890"
-    gen1 = random.choice(chars)
-    gen2 = random.choice(chars)
-    gen3 = random.choice(chars1)
-    gen4 = random.choice(chars)
-    gen5 = random.choice(chars)
-    gen6 = random.choice(chars)
-    gen7 = random.choice(chars1)
-    gen8 = random.choice(chars)
-    gen9 = random.choice(chars)
-    gen10 = random.choice(chars1)
-    word = f"{message.from_user.id}-MERISSA{gen1}{gen2}{gen3}{gen4}{gen5}{gen6}{gen7}{gen8}{gen9}{gen10}"
+    word = f"{message.from_user.id}:{uls(round(message.from_user.id/69696969))}"
     is_user = user.find_one({"user_id": message.from_user.id})
     if not is_user:
         toggle.insert_one({"ID": word, "data": word})
@@ -63,4 +52,4 @@ async def revoke(client, message):
             f"Your Merissa Token: `{MerissaAPI}` Your Merissa Api Token Revoked Successfully.\n\nYou can Generate again using /token."
         )
     else:
-        await message.reply_text("Please First Press /token")
+        await message.reply_text("You don't have any token. Type /token to generate")
