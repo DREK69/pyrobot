49cbec76862b6caa719825e60b6ba54bed5eed06
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-12-11 12:56:26 +0530

    Update ytdl.py

--- MerissaRobot/Modules/ytdl.py
+++ MerissaRobot/Modules/ytdl.py
@@ -487,7 +487,7 @@ async def audio_query(client, callbackquery):
     except:
         album = title
     try:
-        artist = info_dict["artist"]
+        artist = (info_dict["artist"]).split(",")[0]
     except:
         try:
             artist = info_dict["uploader"]
