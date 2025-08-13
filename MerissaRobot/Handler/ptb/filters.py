1b6991ccc9a5a5623234018eb42661dcb4692bee
sᴧɴᴅᴇᴇᴘ sʜᴧʀᴍᴧ <sandeepshrmaxcoder@gmail.com>
2025-08-13 20:33:14 +0530

    Update filters.py

--- MerissaRobot/Handler/ptb/filters.py
+++ MerissaRobot/Handler/ptb/filters.py
@@ -1,48 +1,35 @@
 from telegram import Message
-from telegram.ext import MessageFilter
+from telegram.ext import filters
 
 from MerissaRobot import DEMONS, DEV_USERS, DRAGONS
 
+class CustomFilters:
+    @staticmethod
+    def support_filter(message: Message) -> bool:
+        return bool(message.from_user and message.from_user.id in DEMONS)
 
-class CustomFilters(object):
-    class _Supporters(MessageFilter):
-        def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in DEMONS)
+    @staticmethod
+    def sudo_filter(message: Message) -> bool:
+        return bool(message.from_user and message.from_user.id in DRAGONS)
 
-    support_filter = _Supporters()
+    @staticmethod
+    def dev_filter(message: Message) -> bool:
+        return bool(message.from_user and message.from_user.id in DEV_USERS)
 
-    class _Sudoers(MessageFilter):
-        def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in DRAGONS)
-
-    sudo_filter = _Sudoers()
-
-    class _Developers(MessageFilter):
-        def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in DEV_USERS)
-
-    dev_filter = _Developers()
-
-    class _MimeType(MessageFilter):
-        def __init__(self, mimetype):
-            self.mime_type = mimetype
-            self.name = "CustomFilters.mime_type({})".format(self.mime_type)
-
-        def filter(self, message: Message):
+    @staticmethod
+    def mime_type(mimetype: str):
+        def _filter(message: Message) -> bool:
             return bool(
-                message.document and message.document.mime_type == self.mime_type,
+                message.document and message.document.mime_type == mimetype
             )
+        return _filter
 
-    mime_type = _MimeType
-
-    class _HasText(MessageFilter):
-        def filter(self, message: Message):
-            return bool(
-                message.text
-                or message.sticker
-                or message.photo
-                or message.document
-                or message.video,
-            )
-
-    has_text = _HasText()
+    @staticmethod
+    def has_text(message: Message) -> bool:
+        return bool(
+            message.text
+            or message.sticker
+            or message.photo
+            or message.document
+            or message.video
+        )
