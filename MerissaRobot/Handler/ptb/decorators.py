78b573f019268839cf089b43798442463e916c6b
sᴧɴᴅᴇᴇᴘ sʜᴧʀᴍᴧ <sandeepshrmaxcoder@gmail.com>
2025-08-13 20:29:47 +0530

    Update decorators.py

--- MerissaRobot/Handler/ptb/decorators.py
+++ MerissaRobot/Handler/ptb/decorators.py
@@ -1,15 +1,14 @@
 from typing import List, Optional, Union
-
 from telegram.ext import (
+    Application,
     CallbackQueryHandler,
     CommandHandler,
     InlineQueryHandler,
     MessageHandler,
+    filters,
 )
-from telegram.ext.filters import MessageFilter
 
-from MerissaRobot import LOGGER
-from MerissaRobot import dispatcher as d
+from MerissaRobot import LOGGER, application  # use application, not dispatcher
 from MerissaRobot.Modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
@@ -17,94 +16,83 @@ from MerissaRobot.Modules.disable import (
 
 
 class MerissaHandler:
-    def __init__(self, d):
-        self._dispatcher = d
+    def __init__(self, app: Application):
+        self._application = app
 
     def command(
         self,
-        command: str,
-        filters: Optional[MessageFilter] = None,
+        command: Union[str, List[str]],
+        filters: Optional[filters.BaseFilter] = None,
         admin_ok: bool = False,
-        pass_args: bool = False,
-        pass_chat_data: bool = False,
-        run_async: bool = True,
         can_disable: bool = True,
-        group: Optional[Union[int, str]] = 40,
+        group: Optional[int] = 40,
     ):
         def _command(func):
-            try:
-                if can_disable:
-                    self._dispatcher.add_handler(
-                        DisableAbleCommandHandler(
-                            command,
-                            func,
-                            filters=filters,
-                            run_async=run_async,
-                            pass_args=pass_args,
-                            admin_ok=admin_ok,
-                        ),
-                        group,
-                    )
-                else:
-                    self._dispatcher.add_handler(
-                        CommandHandler(
-                            command,
-                            func,
-                            filters=filters,
-                            run_async=run_async,
-                            pass_args=pass_args,
-                        ),
-                        group,
-                    )
-                LOGGER.debug(
-                    f"[INNEXIACMD] Loaded handler {command} for function {func.__name__} in group {group}"
-                )
-            except TypeError:
-                if can_disable:
-                    self._dispatcher.add_handler(
-                        DisableAbleCommandHandler(
-                            command,
-                            func,
-                            filters=filters,
-                            run_async=run_async,
-                            pass_args=pass_args,
-                            admin_ok=admin_ok,
-                            pass_chat_data=pass_chat_data,
-                        )
-                    )
-                else:
-                    self._dispatcher.add_handler(
-                        CommandHandler(
-                            command,
-                            func,
-                            filters=filters,
-                            run_async=run_async,
-                            pass_args=pass_args,
-                            pass_chat_data=pass_chat_data,
-                        )
-                    )
-                LOGGER.debug(
-                    f"[INNEXIACMD] Loaded handler {command} for function {func.__name__}"
-                )
-
+            handler_cls = DisableAbleCommandHandler if can_disable else CommandHandler
+            self._application.add_handler(
+                handler_cls(command, func, filters=filters),
+                group=group,
+            )
+            LOGGER.debug(
+                f"[MERISSACMD] Loaded handler {command} for function {func.__name__} in group {group}"
+            )
             return func
 
         return _command
 
     def message(
         self,
-        pattern: Optional[str] = None,
+        pattern: Optional[filters.BaseFilter] = None,
         can_disable: bool = True,
-        run_async: bool = True,
-        group: Optional[Union[int, str]] = 60,
+        group: Optional[int] = 60,
         friendly=None,
     ):
         def _message(func):
-            try:
-                if can_disable:
-                    self._dispatcher.add_handler(
-                        DisableAbleMessageHandler(
-                            pattern, func, friendly=friendly, run_async=run_async
+            handler_cls = DisableAbleMessageHandler if can_disable else MessageHandler
+            self._application.add_handler(
+                handler_cls(pattern, func, friendly=friendly),
+                group=group,
+            )
+            LOGGER.debug(
+                f"[MERISSAMSG] Loaded filter pattern {pattern} for function {func.__name__} in group {group}"
+            )
+            return func
+
+        return _message
+
+    def callbackquery(self, pattern: str = None):
+        def _callbackquery(func):
+            self._application.add_handler(
+                CallbackQueryHandler(pattern=pattern, callback=func)
+            )
+            LOGGER.debug(
+                f"[MERISSACALLBACK] Loaded callbackquery handler with pattern {pattern} for function {func.__name__}"
+            )
+            return func
+
+        return _callbackquery
+
+    def inlinequery(
+        self,
+        pattern: Optional[str] = None,
+        chat_types: List[str] = None,
+    ):
+        def _inlinequery(func):
+            self._application.add_handler(
+                InlineQueryHandler(pattern=pattern, callback=func, chat_types=chat_types)
+            )
+            LOGGER.debug(
+                f"[MERISSAINLINE] Loaded inlinequery handler with pattern {pattern} for function {func.__name__} | CHAT TYPES: {chat_types}"
+            )
+            return func
+
+        return _inlinequery
+
+
+merissacmd = MerissaHandler(application).command
+merissamsg = MerissaHandler(application).message
+merissacallback = MerissaHandler(application).callbackquery
+merissainline = MerissaHandler(application).inlinequery                            pattern, func, friendly=friendly, run_async=run_async
                         ),
                         group,
                     )
