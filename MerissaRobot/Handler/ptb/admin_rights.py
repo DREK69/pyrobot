0cc465d6c2f4920b0c3c5e03f380ace8b7c9748d
sᴧɴᴅᴇᴇᴘ sʜᴧʀᴍᴧ <sandeepshrmaxcoder@gmail.com>
2025-08-13 20:08:22 +0530

    Update admin_rights.py upgrade to python-telegram-bot==22.3 Latest version

--- MerissaRobot/Handler/ptb/admin_rights.py
+++ MerissaRobot/Handler/ptb/admin_rights.py
@@ -1,17 +1,18 @@
 from telegram import Chat, User
+from telegram.ext import ContextTypes
 
+async def user_can_promote(chat: Chat, user: User, bot_id: int) -> bool:
+    member = await chat.get_member(user.id)
+    return member.can_promote_members
 
-def user_can_promote(chat: Chat, user: User, bot_id: int) -> bool:
-    return chat.get_member(user.id).can_promote_members
+async def user_can_ban(chat: Chat, user: User, bot_id: int) -> bool:
+    member = await chat.get_member(user.id)
+    return member.can_restrict_members
 
+async def user_can_pin(chat: Chat, user: User, bot_id: int) -> bool:
+    member = await chat.get_member(user.id)
+    return member.can_pin_messages
 
-def user_can_ban(chat: Chat, user: User, bot_id: int) -> bool:
-    return chat.get_member(user.id).can_restrict_members
-
-
-def user_can_pin(chat: Chat, user: User, bot_id: int) -> bool:
-    return chat.get_member(user.id).can_pin_messages
-
-
-def user_can_changeinfo(chat: Chat, user: User, bot_id: int) -> bool:
-    return chat.get_member(user.id).can_change_info
+async def user_can_changeinfo(chat: Chat, user: User, bot_id: int) -> bool:
+    member = await chat.get_member(user.id)
+    return member.can_change_info
