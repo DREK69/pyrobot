a16703aaf8d72ee8872513b7f09eb8bbdaa12b4b
DREK <userbot870@gmail.com>
2025-08-17 19:50:38 +0530

    Update joinreq.py

--- MerissaRobot/Modules/joinreq.py
+++ MerissaRobot/Modules/joinreq.py
@@ -1,9 +1,9 @@
 import html
 import re
 
-from telegram.ext.callbackcontext import CallbackContext
-from telegram.update import Update
-from telegram.utils.helpers import mention_html
+from telegram import Update
+from telegram.ext import ContextTypes
+from telegram.helpers import mention_html
 
 from MerissaRobot.Handler.ptb.chat_status import cuser_admin
 from MerissaRobot.Handler.ptb.decorators import merissacallback
@@ -13,7 +13,7 @@ from MerissaRobot.Modules.log_channel import loggable
 @merissacallback(pattern=r"req_approve=")
 @cuser_admin
 @loggable
-def approve_joinReq(update: Update, context: CallbackContext) -> str:
+async def approve_joinReq(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     bot = context.bot
     query = update.callback_query
     user = update.effective_user
@@ -22,14 +22,16 @@ def approve_joinReq(update: Update, context: CallbackContext) -> str:
 
     user_id = match.group(1)
     try:
-        bot.approve_chat_join_request(chat.id, user_id)
-        joined_user = bot.get_chat_member(chat.id, user_id)
+        await bot.approve_chat_join_request(chat.id, user_id)
+        joined_user = await bot.get_chat_member(chat.id, user_id)
         joined_mention = mention_html(user_id, html.escape(joined_user.user.first_name))
         admin_mention = mention_html(user.id, html.escape(user.first_name))
-        update.effective_message.edit_text(
+        
+        await update.effective_message.edit_text(
             f"{joined_mention}'s join request was approved by {admin_mention}.",
             parse_mode="HTML",
         )
+        
         logmsg = (
             f"<b>{html.escape(chat.title)}:</b>\n"
             f"#JOIN_REQUEST\n"
@@ -39,13 +41,14 @@ def approve_joinReq(update: Update, context: CallbackContext) -> str:
         )
         return logmsg
     except Exception as e:
-        update.effective_message.edit_text(str(e))
+        await update.effective_message.edit_text(str(e))
+        return ""
 
 
 @merissacallback(pattern=r"req_decline=")
 @cuser_admin
 @loggable
-def decline_joinReq(update: Update, context: CallbackContext) -> str:
+async def decline_joinReq(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     bot = context.bot
     query = update.callback_query
     user = update.effective_user
@@ -54,14 +57,16 @@ def decline_joinReq(update: Update, context: CallbackContext) -> str:
 
     user_id = match.group(1)
     try:
-        bot.decline_chat_join_request(chat.id, user_id)
-        joined_user = bot.get_chat_member(chat.id, user_id)
+        await bot.decline_chat_join_request(chat.id, user_id)
+        joined_user = await bot.get_chat_member(chat.id, user_id)
         joined_mention = mention_html(user_id, html.escape(joined_user.user.first_name))
         admin_mention = mention_html(user.id, html.escape(user.first_name))
-        update.effective_message.edit_text(
+        
+        await update.effective_message.edit_text(
             f"{joined_mention}'s join request was declined by {admin_mention}.",
             parse_mode="HTML",
         )
+        
         logmsg = (
             f"<b>{html.escape(chat.title)}:</b>\n"
             f"#JOIN_REQUEST\n"
@@ -71,4 +76,5 @@ def decline_joinReq(update: Update, context: CallbackContext) -> str:
         )
         return logmsg
     except Exception as e:
-        update.effective_message.edit_text(str(e))
+        await update.effective_message.edit_text(str(e))
+        return ""
