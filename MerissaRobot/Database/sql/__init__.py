9c4022336a3b694f2176ecbaf10c1ad51466f430
DREK <userbot870@gmail.com>
2025-08-26 12:01:33 +0530

    Update __init__.py

--- MerissaRobot/Database/sql/__init__.py
+++ MerissaRobot/Database/sql/__init__.py
@@ -8,16 +8,16 @@ from MerissaRobot import LOGGER as log
 if DB_URI and DB_URI.startswith("postgres://"):
     DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
 
-
-def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
-    log.info("[PostgreSQL] Connecting to database......")
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
-
-
 BASE = declarative_base()
+
+def start():
+    global ENGINE
+    ENGINE = create_engine(DB_URI, client_encoding="utf8")
+    log.info("[PostgreSQL] Connecting to database......")
+    BASE.metadata.bind = ENGINE
+    BASE.metadata.create_all(ENGINE)
+    return scoped_session(sessionmaker(bind=ENGINE, autoflush=False))
+
 try:
     SESSION = start()
 except Exception as e:
